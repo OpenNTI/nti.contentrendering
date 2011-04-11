d8ca8d38a605169e9f420ed73609b14f1f421875
Jason Madden <jason.madden@nextthought.com>
2011-04-11 23:46:17 +0000

    Get more counters working. Get problem references working. Get rightpic working exactly as includegraphics.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/AoPS/trunk/src/main/python@4 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- aopsbook.py
+++ aopsbook.py
@@ -35,23 +35,21 @@ class parts(Base.List):
 	args = '[ init:int ]'
 
 	def invoke( self, tex ):
-		print 'Invoking parts'
 		res = super(parts, self).invoke( tex )
-		print self.attributes
+
 		if 'init' in self.attributes and self.attributes['init']:
 			self.ownerDocument.context.counters['partnum'].setcounter( self.attributes['init'] )
 		elif self.macroMode != Base.Environment.MODE_END:
 			self.ownerDocument.context.counters['partnum'].setcounter(0)
 
-	pass
 
 class part(Base.List.item):
-#	args = ''
+	#Ordinary list items can accept a value, this may or may not be used in AoPS code
+	#args = ''
 
 	def invoke( self, tex ):
 		self.counter = 'partnum'
 		self.position = self.ownerDocument.context.counters[self.counter].value + 1
-		print 'Part position %s' % self.position
 		#ignore the list implementation
 		return Command.invoke(self,tex)
 
@@ -62,13 +60,65 @@ class part(Base.List.item):
 class bogus(Base.Environment):
 	pass
 
+from plasTeX.Base import Crossref
+
+class probref(Crossref.ref):
+	pass
+
+class picsecprob(Base.Environment):
+	args = ' pic '
+
+	def invoke( self, tex ):
+		if self.macroMode != Base.Environment.MODE_END:
+			self.ownerDocument.context.counters['probnum'].stepcounter()
+
+
+
 
 from plasTeX.Packages.graphicx import *
 
-#class includegraphics(_OneText):
-#	pass
+class rightpic(includegraphics):
+	" For our purposes, exactly the same as an includegraphics commend. "
+#	packageName = 'aopsbook'
+
+
+	def invoke( self, tex ):
+		# literally replace it with include graphics. This doesn't seem to be quite working at
+		# render time.
+		node = includegraphics()
+		node.parentNode = self.parentNode
+		node.ownerDocument = self.ownerDocument
+		res = node.invoke( tex )
+		if res is None:
+			self._backup = node
+			# FIXME: Returning this as a list (to completely replace this object) doesn't work.
+			# Do we understand that wrong?
+			return node
+		return res
+
+	def __getattribute__(self, value ):
+		# TODO: With replacement working, this is useless
+		if value == 'imageoverride':
+			return getattr( self._backup, value )
+		return includegraphics.__getattribute__( self, value )
+
+
+leftpic = rightpic
+parpic = rightpic
 
 def ProcessOptions( options, document ):
 	document.context.newcounter( 'partnum' )
+	# used in \begin{problem}.
+	# TODO: Get this to reset in chapters (probably not important)
+	document.context.newcounter( 'probnum' )
+
+	# With customising the paths, we could use absolotue paths and fix
+	# the temporary directory issue? Really only important for direct
+	# HTML rendering
+	document.userdata.setPath( 'packages/aopsbook/paths', ['.', '../Images/'])
+	document.userdata.setPath( 'packages/graphicx/paths', ['.', '../Images/'])
+
+	document.userdata.setPath( 'packages/graphicx/extensions',
+							   ['.pdf', '.png','.jpg','.jpeg','.gif','.ps','.eps'])
 
 
