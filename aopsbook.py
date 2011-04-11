1ece45a203b93b2586b104322ea56256dba5ac08
Jason Madden <jason.madden@nextthought.com>
2011-04-11 22:05:54 +0000

    Use a separate counter for parts, as in the .sty, demonstrating how that's done.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/AoPS/trunk/src/main/python@3 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- aopsbook.py
+++ aopsbook.py
@@ -4,8 +4,13 @@ class _OneText(Base.Command):
 	args = 'text:str'
 
 	def invoke( self, tex ):
-		self.parse( tex )
-		print self.__dict__
+		return super(_OneText, self).invoke( tex )
+
+class rindent(Base.Command):
+
+	def invoke( self, tex ):
+		return []
+
 
 class Def(_OneText):
 	pass
@@ -18,10 +23,43 @@ class angle(Base.Command):
 	def invoke( self, tex ):
 		super(angle, self).invoke(  tex )
 
+# Counters
+class partnum(Base.Command):
+	unicode = ''
+
+
+
 class parts(Base.List):
+
+	counters = ['partnum']
+	args = '[ init:int ]'
+
+	def invoke( self, tex ):
+		print 'Invoking parts'
+		res = super(parts, self).invoke( tex )
+		print self.attributes
+		if 'init' in self.attributes and self.attributes['init']:
+			self.ownerDocument.context.counters['partnum'].setcounter( self.attributes['init'] )
+		elif self.macroMode != Base.Environment.MODE_END:
+			self.ownerDocument.context.counters['partnum'].setcounter(0)
+
 	pass
 
 class part(Base.List.item):
+#	args = ''
+
+	def invoke( self, tex ):
+		self.counter = 'partnum'
+		self.position = self.ownerDocument.context.counters[self.counter].value + 1
+		print 'Part position %s' % self.position
+		#ignore the list implementation
+		return Command.invoke(self,tex)
+
+
+	def digest( self, tex ):
+		super( part, self ).digest( tex )
+
+class bogus(Base.Environment):
 	pass
 
 
@@ -29,5 +67,8 @@ from plasTeX.Packages.graphicx import *
 
 #class includegraphics(_OneText):
 #	pass
+
+def ProcessOptions( options, document ):
+	document.context.newcounter( 'partnum' )
 
 
