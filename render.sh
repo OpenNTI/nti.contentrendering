9ad353d6c3e8c46f8acac6cad84c0e372c4eced8
Jason Madden <jason.madden@nextthought.com>
2011-04-18 19:44:53 +0000

    Use an absolute path. Use the local launch script.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/AoPS/trunk/src/main/python@25 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- render.sh
+++ render.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-MYPATH=`dirname $0`
+ORIG_PATH=`pwd`
+cd "$(dirname $0)"
+MYPATH=`pwd`
+cd "$ORIG_PATH"
+
 export XHTMLTEMPLATES=$MYPATH/../renderers
 export PYTHONPATH=$MYPATH:$MYPATH/../plastex/
 
-python2.7 ~/bin/plastex/plasTeX/plastex --xml --save-image-file --split-level=0 --image-compiler=pdflatex --imager=gspdfpng2 --enable-image-cache --vector-imager=pdf2svg $1
+python2.7 $MYPATH/../plastex/plasTeX/plastex --xml --save-image-file --split-level=0 --image-compiler=pdflatex --imager=gspdfpng2 --enable-image-cache --vector-imager=pdf2svg $1
 
