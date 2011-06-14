4d4fc7c0437a74b2734630572780f2e9219ddf6d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2011-06-14 22:00:03 +0000

    add concurrency to resource generation (set to 4)
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/AoPS/trunk/src/main/python@371 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- tex2html.py
+++ tex2html.py
@@ -1,30 +1,144 @@
 #!/usr/bin/env python
 
-import codecs, os, re, pdb, shutil, sys
+import codecs, os, shutil, time
 import resources
-import xml.sax
 import tempfile
-from xml.sax.xmlreader import InputSource
-from xml.dom import minidom
 from StringIO import StringIO
 import subprocess
-from xml.dom import minidom
 import cgi
 from plasTeX.Imagers import *
-import resources
 import html2mathml
+from concurrent.futures import ProcessPoolExecutor
+
+_debug = False
+
+class SetGenerator():
+	
+	htmlfile = 'math.html'
+	
+	illegalCommands=None
+		
+	def __init__(self, compiler, batch):
+		self.batch = batch
+		self.compiler = compiler;
+		self.writer = StringIO()
+		self.generatables = list()
+		
+	def size(self):
+		return len(self.generatables)
+	
+	def addResource(self, s, context=''):
+		self.generatables.append(s)
+		self.writeResource(s, context)
+		
+	def writePreamble(self, document, configName):
+		
+		self.writer.write('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"\
+		"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">\
+		<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">\
+		<head>\
+		<link rel="stylesheet" href="styles/styles.css" />\
+		<script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>')
+
+		if configName:
+			self.writer.write('<script type="text/javascript" src="%s"></script>' % configName)
+		
+		self.writer.write('<script type="text/javascript"\
+		src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>\
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.13/jquery-ui.min.js">\
+		</script>\
+		</head>\
+		<body>')
+		
+	def writeResource(self, source, context):
+		self.writer.write('%s<span class="mathjax math tex2jax_process mathquill-embedded-latex">\(%s\)</span>\n\n' % (context , cgi.escape(source[1:-1])))
+
+	def writePostamble(self):
+		self.writer.write('</body></html>')
+		
+	def processSource(self, sourceConfigPath):
+		
+		start = time.time()
+		
+		output = self.compileSource(sourceConfigPath)
+		
+		files = self.convert(output)
+		nfiles = len(files)
+		
+		if nfiles != len(self.generatables):
+			print 'WARNING.	Expected %s files but only generated %s for batch %s' %\
+				  (len(self.generatables), len(files), self.batch)
+		
+		elapsed = time.time() - start
+		print "%s sources generated in %sms for batch %s" % (nfiles, elapsed, self.batch)
+			
+		return zip(files, self.generatables)
+	
+	def compileSource(self, sourceConfigPath):
+
+		source = self.writer;
+		source.seek(0)
+		htmlSource = source.read()
+		
+		tempdir = tempfile.mkdtemp()
+		
+		#We need to copy the html file
+		htmlOutFile = os.path.join(tempdir, self.htmlfile)
+		codecs.open(htmlOutFile, 'w', 'utf-8').write(htmlSource)
+		
+		configName = os.path.basename(sourceConfigPath);
+		configOutFile = os.path.join(tempdir, configName)
+		try:
+			configOutFile = os.path.join(tempdir, configName)
+			copy(sourceConfigPath, configOutFile)
+			
+			program	 = self.compiler
+			command = '%s "%s"' % (program, htmlOutFile)
+			
+			print 'executing %s' %command
+			stdout, stderr=subprocess.Popen( command, shell=True, stdout=subprocess.PIPE).communicate()
+			
+			if _debug:
+				print 'out'
+				print stdout
+				print 'error'
+				print stderr
+			
+			return stdout	
+		finally:
+			os.remove(configOutFile)
+			
+	def convert(self, output):
+		
+		tempdir = tempfile.mkdtemp()
+		
+		maths = [math.strip().decode('utf-8') for math in output.split('\n') if math.strip()]
+
+		i = 1
+		files=[]
+		for math in maths:
+			fname=os.path.join(tempdir, ('math_%s.xml' % i))
+			i=i+1
+			codecs.open(fname, 'w', 'utf-8').write(math)
+			files.append(fname)
+
+		return files
+	
+#End SetGenerator
+		
+def _processBatchSource(generator, sourceConfigPath):
+	if generator.size() > 0:
+		return generator.processSource(sourceConfigPath);
+	else:
+		return ()
 
 class ResourceGenerator(html2mathml.ResourceGenerator):
 
-	htmlfile = 'math.html'
+	concurrency = 4
 	resourceType = 'mathjax_inline'
-
 	javascript = '%s/tex2html.js'%(os.path.dirname(__file__))
-
 	mathjaxconfigname = 'mathjaxconfig.js'
 	mathjaxconfigfile = '%s/../renderers/Themes/AoPS/js/%s'%(os.path.dirname(__file__), mathjaxconfigname)
-
-	illegalCommands=None
 
 	def __init__(self, document):
 		super(ResourceGenerator, self).__init__(document)
@@ -38,109 +152,58 @@ class ResourceGenerator(html2mathml.ResourceGenerator):
 
 
 	def generateResources(self, document, sources, db):
-		self.source = StringIO()
-
-		self.writePreamble(document)
-
+		
 		generatableSources=[s for s in sources if self.canGenerate(s)]
 
-		if not len(generatableSources) > 0:
+		size = len(generatableSources)
+		if not size > 0:
 			print 'No sources to generate'
 			return
-
-		for s in generatableSources:
-			self.writeResource(s, '')
-
-		self.source.write('</body></html>')
-
-		output = self.compileSource()
-
-
-
-		tempdir = tempfile.mkdtemp()
-
-		cwd=os.getcwd()
-
-		os.chdir(tempdir)
-
-		files = self.convert(output)
-
-		if len(files) != len(generatableSources):
-			print 'WARNING.	 Expected %s files but only generated %s' % (len(generatableSources), len(files))
-
-		os.chdir(cwd)
-
-		for fname, source in zip(files, generatableSources):
-			print '%s  -- %s' % (source, fname)
-			db.setResource(source, [self.resourceType], resources.Resource(os.path.join(tempdir, fname)))
-
-
-	def convert(self, output):
-		maths = [math.strip().decode('utf-8') for math in output.split('\n') if math.strip()]
-
-
-		i = 1
-		files=[]
-		for math in maths:
-			fname='math_%s.xml'%i
-			i=i+1
-			codecs.open(fname, 'w', 'utf-8').write(math)
-			files.append(fname)
-
-		return files
-
-	def writePreamble(self, document):
-		self.source.write('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">\
-		<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">\
-		<head>\
-		<link rel="stylesheet" href="styles/styles.css" />\
-		<script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>')
-
-		if self.mathjaxconfigfile:
-			self.source.write('<script type="text/javascript" src="%s"></script>' % self.mathjaxconfigname)
 		else:
+			print 'Generating %s sources' % size
+			
+		configName = self.mathjaxconfigfile if self.mathjaxconfigname else None
+		if not configName:
 			print 'Mathjax config has not been provided.'
+			
+		generators = list()
+		for i in range(self.concurrency):
+			g = SetGenerator(self.compiler, i)	
+			generators.append(g);	
+			g.writePreamble(document, configName)
+				
+		i = 0
+		for s in generatableSources:
+			g = generators[i]
+			g.addResource(s, '')
+			i = i+1 if (i+1) < self.concurrency else 0
 
-		self.source.write('<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>\
-		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.13/jquery-ui.min.js"></script>\
-		</head>\
-		<body>')
+		for g in generators:
+			g.writePostamble()
 
+		#Process batches in parallel,
+		params = [self.mathjaxconfigfile]*self.concurrency
+		with ProcessPoolExecutor() as executor:
+			for zipped in executor.map( _processBatchSource, generators, params):
+				for fpath, source in zipped:
+					if _debug:
+						print '%s  -- %s' % (source, os.path.basename(fpath))
+					db.setResource(source, [self.resourceType], resources.Resource(fpath))
+			
+	def writePreamble(self, document):
+		pass
+	
+	def convert(self, output):
+		pass
+	
 	def writeResource(self, source, context):
-		self.source.write('%s<span class="mathjax math tex2jax_process mathquill-embedded-latex">\(%s\)</span>\n\n' % (context , cgi.escape(source[1:-1])))
-
+		pass
+	
 	def compileSource(self):
-		cwd = os.getcwd()
-
-		# Make a temporary directory to work in
-		tempdir = tempfile.mkdtemp()
-		os.chdir(tempdir)
-
-
-		#We need to copy the mathjaxconfig file and then reference the relative path
-		copy(self.mathjaxconfigfile, os.path.join(tempdir, self.mathjaxconfigname))
-
-		self.source.seek(0)
-		codecs.open(self.htmlfile, 'w', 'utf-8').write(self.source.read())
-
-		# Run LaTeX
-		os.environ['SHELL'] = '/bin/sh'
-		program	 = self.compiler
-
-		command = '%s %s' % (self.compiler, self.htmlfile)
-		print 'executing %s' %command
-		stdout, stderr=subprocess.Popen( command, shell=True, stdout=subprocess.PIPE).communicate()
-		print 'out'
-		print stdout
-		print 'error'
-		print stderr
-
-		os.remove(self.mathjaxconfigname)
-
-		os.chdir(cwd)
-
-		return stdout
-
+		pass
+	
+#End ResourceGenerator		
+			
 def findfile(file, searchPaths):
 	for dirname in searchPaths:
 		possible = os.path.join(dirname, file)
@@ -150,7 +213,8 @@ def findfile(file, searchPaths):
 
 def copy(source, dest):
 
-	print 'Copying %s to %s' % (source, dest)
+	if _debug:
+		print 'Copying %s to %s' % (source, dest)
 
 	if not os.path.exists(os.path.dirname(dest)):
 		os.makedirs(os.path.dirname(dest))
