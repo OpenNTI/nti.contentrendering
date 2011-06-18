29df72e55e991c326a43a654697499c0f324510e
Chris Utz <chris.utz@nextthought.com>
2011-06-18 18:15:05 +0000

    Add script to determine relative content scroll heights for rendered html.  Add post render step to aopstoxml and incorporate tocinconsetter and contentsizesetter transforms
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/AoPS/trunk/src/main/python@405 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ contentsizesetter.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+
+
+import os
+import re
+import sys
+from xml.dom.minidom import parse
+from xml.dom.minidom import Node
+import subprocess
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+
+tocFileName='eclipse-toc.xml'
+
+javascript = os.path.join('%s'%(os.path.dirname(__file__)),'getContentHeight.js')
+
+contentSizeName = 'NTIRelativeScrollHeight'
+
+def main(args):
+	""" Main program routine """
+
+	if not len(args)>0:
+		print "Usage: contentsizesetter.py path/to/content"
+		sys.exit()
+
+	transform(args.pop(0))
+
+def transform(contentLocation):
+	"""
+	Use the toc file to find all the pages in the contentLocation.
+	Use phantomjs and js to render the page and extract the content size.
+	Stuff the contentsize in the page as a meta tag and add it to toc
+	"""
+	tocFile = os.path.join(contentLocation, tocFileName)
+
+	print 'Using %s' % tocFile
+
+	tocDOM = parse(tocFile)
+
+	#Build a list of files we need to do
+	rootTOC = tocDOM.getElementsByTagName('toc')[0]
+
+	htmlFiles = []
+	findHTMLInTOC(rootTOC, htmlFiles);
+
+	htmlFiles = [os.path.join(contentLocation, f) for f in htmlFiles]
+
+	print 'Finding content size for %s' % htmlFiles
+
+	contentSizes = {}
+
+	#Generate sizes for all our pages
+	with ProcessPoolExecutor() as executor:
+			for the_tuple in executor.map( getContentSize, htmlFiles):
+				contentSizes[the_tuple[0]]=the_tuple[1]
+
+	print 'Storing generated content sizes'
+	storeContentSizes(contentSizes, rootTOC, contentLocation)
+
+
+	f = open(tocFile, 'w')
+	rootTOC.writexml(f)
+	f.close()
+
+def storeContentSizes(contentSizes, node, contentLocation):
+
+	htmlFile = os.path.join(contentLocation, node.getAttribute('href'))
+	contentSize = contentSizes[htmlFile]
+
+	writeContentSizeToMeta(htmlFile, contentSize)
+	node.attributes[contentSizeName]=contentSize
+
+	for child in node.childNodes:
+		if getattr(child, 'hasAttributes', None) and child.hasAttribute('href'):
+			storeContentSizes(contentSizes,child, contentLocation);
+
+
+
+
+
+def writeContentSizeToMeta(htmlFile, contentSize):
+	if htmlFile.startswith('./'):
+		htmlFile = htmlFile[2:]
+
+	command = 'sed -i .bkp \
+					  \"s/\\(<meta name=\\"NTIRelativeScrollHeight\\" content=\\"\\).*\\(\\" \\/>\\)/\\1%s\\2/\" %s' % (contentSize,htmlFile)
+
+	#print command
+
+	subprocess.Popen(command, shell=True)
+
+	backupFile = htmlFile+'.bkp'
+
+	if os.path.exists(backupFile):
+		os.remove(htmlFile+'.bkp')
+
+def getContentSize(htmlFile):
+	height = subprocess.Popen( "phantomjs %s %s" % (javascript, htmlFile), shell=True, stdout=subprocess.PIPE).communicate()[0].strip()
+	return (htmlFile, height)
+
+def findHTMLInTOC(toc, files):
+	if toc.hasAttribute('href'):
+		files.append(toc.getAttribute('href'))
+
+	for node in toc.getElementsByTagName('topic'):
+		findHTMLInTOC(node, files);
+
+if __name__ == '__main__':
+	main(sys.argv[1:])
