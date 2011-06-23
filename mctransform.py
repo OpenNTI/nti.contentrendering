35bfd85803d1642608e8ba217c256affa38a0316
Carlos Sanchez <carlos.sanchez@nextthought.com>
2011-06-23 14:04:39 +0000

    send content to server
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/AoPS/trunk/src/main/python@464 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- mctransform.py
+++ mctransform.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os, sys
+import httplib
 from json import JSONEncoder
 import plasTeX
 from plasTeX.TeX import TeX
@@ -27,13 +28,13 @@ def _parseLaTeX(file):
 	return document
 # End _parseLaTeX
 
-def todict(file):
+def todict(file, idprefix = ''):
 	"""
 	Return a python dictionary from the specified LaTeX file
 	"""
-	return _todict(_parseLaTeX(file))
+	return _todict(_parseLaTeX(file), idprefix)
 	
-def _todict(document):
+def _todict(document, idprefix = ''):
 	"""
 	Return a python dictionary from the specified plasTeX document
 	"""
@@ -55,24 +56,24 @@ def _todict(document):
 			answers.append(solution.source)
 			
 		if problemSource and answers:
-			qsid = str(questionId)
+			qsid = idprefix + str(questionId)
 			items[qsid] = (qsid, problemSource, answers)
 			
 		questionId = questionId + 1
 	
 	return result
 
-def toJSON(file):
+def toJSON(file, idprefix = ''):
 	"""
 	Return a JSON string from the specified LaTeX file
 	"""
-	return _toJSON(_parseLaTeX(file))
+	return _toJSON(_parseLaTeX(file), idprefix)
 	
-def _toJSON(document):
+def _toJSON(document, idprefix = ''):
 	"""
 	Return a JSON string from the specified plasTeX document
 	"""
-	return JSONEncoder().encode(_todict(document))
+	return JSONEncoder().encode(_todict(document, idprefix))
 
 def toXml( file, outfile = None ):
 	"""
@@ -106,10 +107,12 @@ def transform(file, format='json', outfile = None):
 def put(file, url):
 	"""
 	Put the json representation of the specified LaTeX file in the specified url
-	"""
-	import urllib2
-	
+	"""	
 	json = toJSON(file)
+	return _put(json, url)
+
+def _put(json, url):
+	import urllib2
 	
 	opener = urllib2.build_opener(urllib2.HTTPHandler)
 	request = urllib2.Request(url, data=json)
@@ -118,27 +121,35 @@ def put(file, url):
 	
 	return opener.open(request)
 
-def put2(file, url):
+def put2(file, url, idprefix = ''):
 	"""
 	Put the json representation of the specified LaTeX file in the specified url
 	"""
-	import httplib
 	from urlparse import urlparse
 
 	pr = urlparse(url)
-	server = pr.netloc
-	path = pr.path
-
-	connection =  httplib.HTTPConnection(server)
-	json = toJSON(file)
-	connection.request('PUT', path, json)
+	return _put2(file, pr.netloc, pr.path, idprefix)
 	
+def _put2(file, server, path, idprefix = ''):
+	json = toJSON(file, idprefix)
+	connection =  httplib.HTTPConnection(server)
+	connection.request('PUT', path, json)
 	return connection.getresponse()
 	
+def send2server(file, id, server='http://curie.bacl:8080', path="/dataserver/quizzes/", idprefix = ''):
+	from urlparse import urljoin
+	idpath = urljoin(path, id)
+	_put2(file, server, idpath)
+
 if __name__ == '__main__':
 	args = sys.argv[1:]
 	if args:
-		put2(args[0], 'http://www.nt.com/dataserver/carlos/qid')
+		f = lambda a, idx, d: d if len(a) <= idx else a[idx] 
+		id 		= f(args, 1, 'mathcounts-2011-0')
+		server	= f(args, 2, 'http://curie.bacl:8080')
+		path	= f(args, 3, '/dataserver/quizzes/')
+		idprx	= f(args, 4, '') 
+		send2server(args[0], id, server, path, idprx)
 		
 		
 		
