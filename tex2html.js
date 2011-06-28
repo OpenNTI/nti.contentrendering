65e9c3165d3e1a96aa6bceb3fa616ac9145a50c1
Chris Utz <chris.utz@nextthought.com>
2011-06-28 18:59:45 +0000

    Find math elements whose font size is > 100% and set it back down to 100%.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/AoPS/trunk/src/main/python@541 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- tex2html.js
+++ tex2html.js
@@ -1,18 +1,38 @@
+
 if (phantom.state.length === 0) {
     phantom.state = 'math';
     phantom.open(phantom.args[0]);
 } else {
     MathJax.Hub.Queue(function(){
-jQuery.fn.outerhtml=function outerHTML(){
-	return jQuery("&lt;p&gt;").append(this.eq(0).clone()).html();
-}
+						  jQuery.fn.outerhtml=function outerHTML(){
+							  return jQuery("&lt;p&gt;").append(this.eq(0).clone()).html();
+						  };
+
+						  //If we have mathjax that has been scaled above 100% back it off
+						  $("span.math span[style*='font-size:']").each(function()
+																			{
+																				mathEl = this;
+																				sizeStr = mathEl.style.fontSize;
+
+																				if( sizeStr.charAt(sizeStr.length - 1) ){
+																					var sizeInt = parseInt(sizeStr.substring(0, sizeStr.length - 1));
+																					if(sizeInt && sizeInt > 100){
+																						mathEl.style.fontSize = '100%';
+
+																					}
+																				}
+
+
+
+																			});
+
 						  $(".tex2jax_process").each(function(){
 								span = $(this);
     													 span.children("script").remove();
 																	   console.log( jQuery("<p>").append(span.eq(0).clone()).html());
 						  });
 		phantom.exit();
-	})
+					  });
 }
 
 
