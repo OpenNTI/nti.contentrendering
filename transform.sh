c7295a9437906a77017aae059a44764746f33e1b
Jason Madden <jason.madden@nextthought.com>
2011-07-14 20:31:19 +0000

    Dont trounce the current PYTHONPATH
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/AoPS/trunk/src/main/python@704 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- transform.sh
+++ transform.sh
@@ -2,7 +2,7 @@
 
 MYPATH=`dirname $0`
 export XHTMLTEMPLATES=$MYPATH/../renderers
-export PYTHONPATH=$MYPATH:$MYPATH/../plastex/
+export PYTHONPATH=$MYPATH:$MYPATH/../plastex/:$PYTHONPATH
 
 
 ${PYTHON:-python2.7} $MYPATH/aopstoxml $1 $2
