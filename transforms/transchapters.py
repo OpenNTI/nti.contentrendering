b4b9abb3d1e33d5318c8a9c62ca48835169c394a
Chris Utz <chris.utz@nextthought.com>
2011-08-19 21:41:37 +0000

    No longer need to skip inital chapter quote and author
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/AoPS/trunk/src/main/python@1211 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- transforms/transchapters.py
+++ transforms/transchapters.py
@@ -5,9 +5,8 @@ import traceback
 
 def transform(document):
 	# Chapterauthor and chapterquote need to move down a level, inside
-	# their respective chapters. NOTE: They use the same
-	# macros for the book quote, the first one, which we want to leave alone.
-	for chapterquote in document.getElementsByTagName( 'chapterquote')[1:]:
+	# their respective chapters.
+	for chapterquote in document.getElementsByTagName( 'chapterquote'):
 		chapterauthor = chapterquote.parentNode.getElementsByTagName( 'chapterauthor' )[0]
 		parent = chapterquote.parentNode
 		# move it up out of containing par and raggedbottom, etc
