6af6b4e5fa1a9b3e6ee486084aca8f9d450a96f4
Chris Utz <chris.utz@nextthought.com>
2011-09-30 16:03:30 +0000

    fix thumbnail path in toc
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/AoPS/trunk/src/main/python@2251 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- contentthumbnails.py
+++ contentthumbnails.py
@@ -68,7 +68,7 @@ def transform(book):
 		for ntiid, output in executor.map( _generatedImage,[cwd for x in pageAndOutput], [x[0] for x in pageAndOutput], [x[1] for x in pageAndOutput]):
 			thumbnail = os.path.join(thumbnails, output)
 			copy(os.path.join(tempdir, output), os.path.join(cwd, thumbnail))
-			eclipseTOC.getPageNodeWithNTIID(ntiid).attributes['thumbnail']=thumbnail
+			eclipseTOC.getPageNodeWithNTIID(ntiid).attributes['thumbnail']=os.path.relpath(thumbnail)
 
 	os.chdir(cwd)
 
