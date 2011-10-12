9b586ac57fe70889d1627b37f305c32b34bb0eeb
Carlos Sanchez <carlos.sanchez@nextthought.com>
2011-10-12 19:44:04 +0000

    clean up a bit
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/AoPS/trunk/src/main/python@2479 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- transforms/__init__.py
+++ transforms/__init__.py
@@ -1,23 +1,27 @@
+import os
+import sys
+import pdb
+import glob
 
-import pdb, os, sys, glob
 from plasTeX.Logging import getLogger
 
-log = getLogger()
+logger = getLogger()
 
 def performTransforms(document):
 	transformsDir = os.path.dirname(__file__)
 
-	modules = [os.path.splitext(os.path.basename(fileName))[0] for fileName in \
-			   glob.glob(os.path.join(transformsDir, '*.py')) \
-			   if not '__init__' in fileName]
+	modules = [os.path.splitext(os.path.basename(fileName))[0] \
+				for fileName in glob.glob(os.path.join(transformsDir, '*.py'))  if not '__init__' in fileName]
 
 	for moduleName in modules:
-		module = 'transforms.%s' % moduleName
+		_name = 'transforms.%s' % moduleName
 		try:
-			exec('from %s import transform as transform' % module)
-		except ImportError, msg:
-			log.error('Could not import transforms from "%s".  Make sure that it is installed correctly, and can be imported by Python.' % module)
+			_name = 'transforms.%s' % moduleName
+			_module = __import__(_name, globals(), locals(), ['transform'], -1)
+		except ImportError:
+			logger.error('Could not import transforms from "%s".' % _name)
 			continue
 
-		print 'Running transform for %s' % module
-		transform(document)
+		print 'Running transform for %s' % _name
+		
+		_module.transform(document)
