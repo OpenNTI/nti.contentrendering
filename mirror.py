2bbd1ef8fbfbb42e2fab3f8829c0f110c86e735b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2011-10-25 01:32:48 +0000

    process page nodes
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/AoPS/trunk/src/main/python@2882 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- mirror.py
+++ mirror.py
@@ -23,16 +23,70 @@ def main(url, out_dir="/tmp"):
 		os.makedirs(out_dir)
 		
 	if _get_all_content(url, out_dir) and _get_toc_file(url, out_dir):
-		pass
+		toc_file = os.path.join(out_dir, 'eclipse-toc.xml')
+		_process_toc_file(url, out_dir, toc_file)
 	
 def _process_toc_file(url, out_dir, toc_file='eclipse-toc.xml'):
+	result = True
 	dom = parse(toc_file)
 	toc = dom.getElementsByTagName("toc");
 	if toc and _process_node(toc[0], url, out_dir):
-		pass
-	
+		for node in toc[0].childNodes:
+			if node.nodeType == Node.ELEMENT_NODE and node.localName == 'topic':
+				result = _process_topic(node,  url, out_dir) and result
+	else:
+		result = False
+		
+	return result
+
+def _process_topic(topic, url, out_dir):
+	result = _process_node(topic, url, out_dir)
+	if result:
+		for node in topic.childNodes:
+			if node.nodeType == Node.ELEMENT_NODE:
+				if node.localName == 'topic':
+					result = _process_topic(node,  url, out_dir) and result
+				elif node.localName == 'Related':
+					for c in node.childNodes:
+						if c.nodeType == Node.ELEMENT_NODE and c.localName == 'page':
+							result = _process_node(c,  url, out_dir) and result
+	return result
+				
 def _process_node(node, url, out_dir):
-	return True
+	result = True
+	attributes = node.attributes
+	for i, name  in enumerate (['href', 'qualifier', 'icon', 'thumbnail']):
+		av =  attributes.get(name, None)
+		if av and av.value:
+			if i <= 1 and not av.value.endswith(".html"):
+				continue
+			result = _handle_attribute(av.value, url, out_dir, i<=1) and result
+	return result
+
+def _handle_attribute(attr, url, out_dir, force_html=False):
+	
+	target = os.path.join(out_dir, attr)
+	if os.path.exists(target):
+		return True
+
+	head, _ = os.path.split(attr)
+	out_dir = os.path.join(out_dir, head)
+	if not os.path.exists(out_dir):
+		os.makedirs(out_dir)
+	
+	args = [WGET_CMD, '-N', '-nH', '-P %s' % out_dir]
+	if force_html:
+		args.extend(['-p', '--force-html'])
+		
+	cut_dirs = _get_cut_dirs(url)
+	if cut_dirs > 0:
+		args.append('--cut-dirs=%s' % cut_dirs)
+		
+	url = urljoin(url + '/', attr)
+	args.append(url)
+	_execute_cmd(args)
+	
+	return os.path.exists(target)
 	
 def _get_cut_dirs(url):
 	r = urlparse(url)
@@ -50,11 +104,7 @@ def _get_file(url, out_dir, target):
 		args.append('--cut-dirs=%s' % cut_dirs)
 	args.append(url)
 	
-	cmd = ' '.join(args)
-	retcode = subprocess.call(cmd, shell=True)
-	if retcode != 0:
-		raise Exception("Fail to execute '%s'" % cmd)
-	
+	_execute_cmd(args)
 	return os.path.exists(os.path.join(out_dir, target))
 
 def _get_toc_file(url, out_dir, toc_file='eclipse-toc.xml'):
@@ -80,13 +130,16 @@ def _get_all_content(url, out_dir="/tmp"):
 	args.append('-P %s' % out_dir)
 	args.append(url)
 	
+	_execute_cmd(args)
+	return glob.glob(out_dir + "*.html") > 0
+
+def _execute_cmd(args):
 	cmd = ' '.join(args)
 	retcode = subprocess.call(cmd, shell=True)
 	if retcode != 0:
 		raise Exception("Fail to execute '%s'" % cmd)
 	
-	return glob.glob(out_dir + "*.html") > 0
-
+	return True
 if __name__ == '__main__':
 	args = sys.argv[1:]
 	if args:
