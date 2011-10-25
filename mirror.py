920116ce2ae5f4beaa452fd9c66e6a5daf895062
Carlos Sanchez <carlos.sanchez@nextthought.com>
2011-10-25 16:37:51 +0000

    complete mirror
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/AoPS/trunk/src/main/python@2893 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- mirror.py
+++ mirror.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import glob
+import tempfile
+import html5lib
 import subprocess
 
 from urlparse import urljoin
@@ -9,24 +11,69 @@ from urlparse import urlparse
 from xml.dom.minidom import parse
 from xml.dom.minidom import Node
 
-WGET_CMD = '/opt/local/bin/wget'
+WGET_CMD = 'wget'
 
 def main(url, out_dir="/tmp"):
 	""" 
 	Main program routine 
 	"""
+	global WGET_CMD
 	
 	url = url[:-1] if url[-1] =='/' else url
 	
 	out_dir = os.path.expanduser(out_dir)
 	if not os.path.exists(out_dir):
 		os.makedirs(out_dir)
-		
-	if _get_all_content(url, out_dir) and _get_toc_file(url, out_dir):
-		toc_file = os.path.join(out_dir, 'eclipse-toc.xml')
-		_process_toc_file(url, out_dir, toc_file)
 	
+	log_file = '%s/_out.log' % out_dir
+	_remove_file(log_file)
+	
+	WGET_CMD = "%s -a %s" % (WGET_CMD, log_file)
+
+	result = _get_url_content(url, out_dir) and _get_toc_file(url, out_dir)	
+	if result:
+		toc_file = os.path.join(out_dir, 'eclipse-toc.xml')
+		result = _process_toc_file(url, out_dir, toc_file)
+		if result:
+			_get_index_dir(url, out_dir)
+		
+	return result
+
+def _remove_file(target):
+	try:
+		os.remove(target)
+	except:
+		pass
+	
+def _get_index_dir(url, out_dir):
+	
+	print "Getting [book]/index content"
+	
+	tmp = tempfile.mktemp(".html", "index", out_dir)
+	try:
+		url2 = urljoin(url + '/', 'indexdir')
+		args = [WGET_CMD, '-O %s' % tmp, url2]
+		_execute_cmd(args)	
+	
+		with open(tmp, "r") as source:
+			doc = html5lib.parse(source, encoding="utf-8")
+			for node in doc:
+				if node.type == 5 and node.name == 'a':
+					attributes = node.attributes
+					href = attributes.get('href', None)
+					if href and (href.startswith('_') or href.endswith('_WRITELOCK')):
+						target = "indexdir/" + href
+						_get_file(url, os.path.join(out_dir,"indexdir"), target, False)
+		
+	except Exception, e:
+		print e
+	finally:
+		_remove_file(tmp)
+
 def _process_toc_file(url, out_dir, toc_file='eclipse-toc.xml'):
+	
+	print "Processing TOC content"
+	
 	result = True
 	dom = parse(toc_file)
 	toc = dom.getElementsByTagName("toc");
@@ -63,17 +110,24 @@ def _process_node(node, url, out_dir):
 			result = _handle_attribute(av.value, url, out_dir, i<=1) and result
 	return result
 
-def _handle_attribute(attr, url, out_dir, force_html=False):
+def _handle_attribute(target, url, out_dir, force_html=False):
 	
-	target = os.path.join(out_dir, attr)
-	if os.path.exists(target):
-		return True
-
-	head, _ = os.path.split(attr)
+	head, _ = os.path.split(target)
 	out_dir = os.path.join(out_dir, head)
 	if not os.path.exists(out_dir):
 		os.makedirs(out_dir)
 	
+	return _get_file(url, out_dir, target, force_html)
+	
+def _get_cut_dirs(url):
+	r = urlparse(url)
+	s = r.path.split('/')
+	return len(s) - 1
+	
+def _get_file(url, out_dir, target, force_html=False):
+	"""
+	Return the specified target from the specified url
+	"""
 	args = [WGET_CMD, '-N', '-nH', '-P %s' % out_dir]
 	if force_html:
 		args.extend(['-p', '--force-html'])
@@ -82,39 +136,24 @@ def _handle_attribute(attr, url, out_dir, force_html=False):
 	if cut_dirs > 0:
 		args.append('--cut-dirs=%s' % cut_dirs)
 		
-	url = urljoin(url + '/', attr)
-	args.append(url)
-	_execute_cmd(args)
-	
-	return os.path.exists(target)
-	
-def _get_cut_dirs(url):
-	r = urlparse(url)
-	s = r.path.split('/')
-	return len(s) - 1
-	
-def _get_file(url, out_dir, target):
-	"""
-	return the specified target from the specified url
-	"""
 	url = urljoin(url + '/', target)
-	args = [WGET_CMD, '--force-html', '-nH', '-p', '-P %s' % out_dir]
-	cut_dirs = _get_cut_dirs(url)
-	if cut_dirs > 0:
-		args.append('--cut-dirs=%s' % cut_dirs)
 	args.append(url)
-	
 	_execute_cmd(args)
+	
+	target = os.path.split(target)[1]
 	return os.path.exists(os.path.join(out_dir, target))
 
 def _get_toc_file(url, out_dir, toc_file='eclipse-toc.xml'):
-	return _get_file(url, out_dir, toc_file)
+	print "Getting TOC file"
+	return _get_file(url, out_dir, toc_file, True)
 	
-def _get_all_content(url, out_dir="/tmp"):	
+def _get_url_content(url, out_dir="/tmp"):	
 	"""
 	Get as much as possible content from the
 	specified URL
 	"""
+	
+	print "Getting URL content"
 	
 	# -m   --mirror
 	# -nH  no-host-directories
@@ -140,6 +179,7 @@ def _execute_cmd(args):
 		raise Exception("Fail to execute '%s'" % cmd)
 	
 	return True
+
 if __name__ == '__main__':
 	args = sys.argv[1:]
 	if args:
