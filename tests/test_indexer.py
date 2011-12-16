fe8943c83db5995d08e680d4b54c77a09602391a
Carlos Sanchez <carlos.sanchez@nextthought.com>
2011-12-16 04:43:17 +0000

    test index construction and search
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/src/main/python/nti/contentrendering@3925 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- tests/test_indexer.py
+++ tests/test_indexer.py
@@ -3,11 +3,14 @@ import shutil
 import tempfile
 import unittest
 
+from nti.contentsearch import Book
 from nti.contentrendering.indexer import get_microdata
 from nti.contentrendering.indexer import index_content
 from nti.contentrendering.indexer import get_or_create_index
 
 from hamcrest import assert_that, has_length, is_
+
+from whoosh.query import (Or, Term)
 
 class TestIndexer(unittest.TestCase):	
 	
@@ -25,7 +28,7 @@ class TestIndexer(unittest.TestCase):
 	def test_get_microdata_string(self):
 		raw = """
 			<html>
-			<a name="one" itemscope itemtype="http://schema.org/CreativeWork">what a fuck is going on
+			<a name="one" itemscope itemtype="http://schema.org/CreativeWork">
 			<span itemprop="fraction">.</span>
 			<span itemprop="multiplication"/>
 			<div><h1>to enter</h1></div>
@@ -60,7 +63,20 @@ class TestIndexer(unittest.TestCase):
 	def test_index_content(self):
 		toc = os.path.join(os.path.dirname(__file__), 'eclipse-toc-complete.xml')
 		index_content(toc, indexdir=self.idxdir, optimize=False)
-		get_or_create_index(indexdir=self.idxdir)
+		idx = get_or_create_index(indexdir=self.idxdir, recreate=False)
+		
+		q = Term("keywords", u"mathcounts")
+		with idx.searcher() as s:
+			r = s.search(q)
+			assert_that(r, has_length(1))
+		
+		q = Or([Term("content", u'rules'), Term("keywords", u"number theory")])
+		with idx.searcher() as s:
+			r = s.search(q)
+			print len(r)
+			assert_that(r, has_length(2))
+			
+		idx.close()
 		
 if __name__ == '__main__':
 	unittest.main()
