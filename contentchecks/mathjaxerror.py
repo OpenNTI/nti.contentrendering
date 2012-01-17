ed352ee1307727d5274b3fb3987df6d08aab48e5
Jason Madden <jason.madden@nextthought.com>
2012-01-17 16:16:16 +0000

    Heuristic to catch bad mathjax macros: things that display funny but are not actually errors as far as mathjax is concerned.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/src/main/python/nti/contentrendering@4423 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- contentchecks/mathjaxerror.py
+++ contentchecks/mathjaxerror.py
@@ -11,12 +11,31 @@ interface.moduleProvides(interfaces.IRenderedBookValidator)
 
 def check(book):
 
-	def _check( page ):
-		errors = page.dom( "span[class=merror]" )
+	def _report_errors( errors, page, text='xml' ):
 		all_errors = len(errors)
-		if len(errors):
-			errors = [etree.tostring(error) for error in errors]
+		if all_errors:
+			errors = set([etree.tostring(error,method=text) for error in errors])
 			logger.warn( "Mathjax errors for page %s: %s", page.filename, errors )
+		return all_errors
+
+	def _check_merror( page ):
+		errors = page.dom( "span[class=merror]" )
+		return _report_errors( errors, page )
+
+	def _check_mtext( page ):
+		mtexts = page.dom( "span[class=mtext]" )
+		errors = []
+		for mtext in mtexts:
+			if 'color:' in mtext.attrib['style'] and 'red' in mtext.attrib['style']:
+				# This is a really tacky way of checking for 'color: red'
+				errors.append( mtext )
+
+		return _report_errors( errors, page, text='text' )
+
+
+	def _check( page ):
+		all_errors = _check_merror( page )
+		all_errors += _check_mtext( page )
 		for child in page.childTopics:
 			all_errors += _check( child )
 
