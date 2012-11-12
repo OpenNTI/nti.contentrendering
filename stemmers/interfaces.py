89b9567149d9ac20c58a345beac7c8061daec67c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2012-11-12 23:44:04 +0000

    Move content processing routines to a new module.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver/src/nti/contentrendering@13930 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- stemmers/interfaces.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals, print_function
-
-from zope import interface
-
-class IStemmer(interface.Interface):
-	def stem(token):
-		"""get the stem of the specified token"""
--- taggers/__init__.py
+++
@@ -1,3 +0,0 @@
-from __future__ import print_function, unicode_literals
-
-from nti.contentrendering.taggers._nltk import get_backoff_ngram_tagger
