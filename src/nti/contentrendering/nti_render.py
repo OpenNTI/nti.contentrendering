e30b2433d4961f4817f20110f23c13091ca51c72
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-01-16 16:31:49 +0000

    pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentrendering/trunk@104430 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/nti_render.py
+++ src/nti/contentrendering/nti_render.py
@@ -48,213 +48,224 @@ from nti.contentrendering.resources.resourcetypeoverrides import ResourceTypeOve
 
 DEFAULT_LOG_FORMAT = '[%(asctime)-15s] [%(name)s] %(levelname)s: %(message)s'
 
+
 def _configure_logging(level='INFO', fmt=DEFAULT_LOG_FORMAT):
-	numeric_level = getattr(logging, level.upper(), None)
-	numeric_level = logging.INFO if not isinstance(numeric_level, int) else numeric_level
-	logging.basicConfig(level=numeric_level)
-	logging.root.handlers[0].setFormatter(zope.exceptions.log.Formatter(fmt))
+    level = getattr(logging, level.upper(), None)
+    level = logging.INFO if not isinstance(level, int) else level
+    logging.basicConfig(level=level)
+    logging.root.handlers[0].setFormatter(zope.exceptions.log.Formatter(fmt))
+
 
 def _catching(f):
-	@functools.wraps(f)
-	def y():
-		try:
-			f()
-		except subprocess.CalledProcessError as spe:
-			logger.exception("Failed to run subprocess")
-			sys.exit(spe.returncode)
-		except:
-			logger.exception("Failed to run main")
-			sys.exit(1)
-	return y
+    @functools.wraps(f)
+    def y():
+        try:
+            f()
+        except subprocess.CalledProcessError as spe:
+            logger.exception("Failed to run subprocess")
+            sys.exit(spe.returncode)
+        except:
+            logger.exception("Failed to run main")
+            sys.exit(1)
+    return y
+
 
 def _set_argparser():
-	arg_parser = argparse.ArgumentParser(description="Render NextThought content.")
-	arg_parser.add_argument('contentpath',
-							help="Path to top level content file.")
-	arg_parser.add_argument('-c', '--config',
-							help='Used by render_content wrapper. Ignore if running '
-								 'nti_render standalone.')
-	arg_parser.add_argument('--nochecking',
-							 action='store_true',
-							 default=False,
-							 help="Perform content checks.")
-	arg_parser.add_argument('-o', '--outputformat',
-							 default='xhtml',
-							 help="Output format for rendered files. Default is xhtml")
-	arg_parser.add_argument('--loglevel',
-							 default='INFO',
-							 help="Set logging level to INFO, DEBUG, WARNING, ERROR or "
-							 	  "CRITICAL. Default is INFO.")
-	return arg_parser
+    arg_parser = argparse.ArgumentParser(description="Render NextThought content.")
+    arg_parser.add_argument('contentpath',
+                            help="Path to top level content file.")
+    arg_parser.add_argument('-c', '--config',
+                            help='Used by render_content wrapper. Ignore if running '
+                            'nti_render standalone.')
+    arg_parser.add_argument('--nochecking',
+                            action='store_true',
+                            default=False,
+                            help="Perform content checks.")
+    arg_parser.add_argument('-o', '--outputformat',
+                            default='xhtml',
+                            help="Output format for rendered files. Default is xhtml")
+    arg_parser.add_argument('--loglevel',
+                            default='INFO',
+                            help="Set logging level to INFO, DEBUG, WARNING, ERROR or "
+                            "CRITICAL. Default is INFO.")
+    return arg_parser
+
 
 @_catching
 def main():
-	"""
-	Main program routine
-	"""
-	argv = sys.argv[1:]
-	arg_parser = _set_argparser()
-	args = arg_parser.parse_args(args=argv)
+    """
+    Main program routine
+    """
+    argv = sys.argv[1:]
+    arg_parser = _set_argparser()
+    args = arg_parser.parse_args(args=argv)
 
-	sourceFile = args.contentpath
-	_configure_logging(args.loglevel)
-	dochecking = not args.nochecking
-	outFormat = args.outputformat
+    sourceFile = args.contentpath
+    _configure_logging(args.loglevel)
+    dochecking = not args.nochecking
+    outFormat = args.outputformat
 
-	logger.info("Start rendering for %s", sourceFile)
-	start_t = time.time()
-	document, components, jobname, _ = parse_tex(sourceFile, outFormat=outFormat)
+    logger.info("Start rendering for %s", sourceFile)
+    start_t = time.time()
+    document, components, jobname, _ = parse_tex(
+        sourceFile, outFormat=outFormat)
 
-	db = None
-	if outFormat in ('images', 'xhtml', 'text'):
-		logger.info("Generating images")
-		db = generateImages(document)
+    db = None
+    if outFormat in ('images', 'xhtml', 'text'):
+        logger.info("Generating images")
+        db = generateImages(document)
 
-	if outFormat == 'xhtml':
-		logger.info("Begin render")
-		render(document, document.config['general']['renderer'], db)
-		logger.info("Begin post render")
-		postRender(document,
-				   jobname=jobname,
-				   context=components,
-				   dochecking=dochecking)
-	elif outFormat == 'xml':
-		logger.info("To Xml.")
-		toXml(document, jobname)
+    if outFormat == 'xhtml':
+        logger.info("Begin render")
+        render(document, document.config['general']['renderer'], db)
+        logger.info("Begin post render")
+        postRender(document,
+                   jobname=jobname,
+                   context=components,
+                   dochecking=dochecking)
+    elif outFormat == 'xml':
+        logger.info("To Xml.")
+        toXml(document, jobname)
 
-	elif outFormat == 'text':
-		logger.info("Begin render")
-		render(document, document.config['general']['renderer'], db)
+    elif outFormat == 'text':
+        logger.info("Begin render")
+        render(document, document.config['general']['renderer'], db)
 
-	logger.info("Write metadata.")
-	write_dc_metadata(document, jobname)
+    logger.info("Write metadata.")
+    write_dc_metadata(document, jobname)
 
-	elapsed = time.time() - start_t
-	logger.info("Rendering took %s(s)", elapsed)
+    elapsed = time.time() - start_t
+    logger.info("Rendering took %s(s)", elapsed)
+
 
 def postRender(document,
-			   contentLocation='.',
-			   jobname='prealgebra',
-			   context=None,
-			   dochecking=True):
-	# FIXME: This was not particularly well thought out. We're using components,
-	# but named utilities, not generalized adapters or subscribers.
-	# That makes this not as extensible as it should be.
+               contentLocation='.',
+               jobname='prealgebra',
+               context=None,
+               dochecking=True):
+    # FIXME: This was not particularly well thought out. We're using components,
+    # but named utilities, not generalized adapters or subscribers.
+    # That makes this not as extensible as it should be.
 
-	# We very likely will get a book that has no pages
-	# because NTIIDs are not added yet.
-	start_t = time.time()
-	logger.info('Creating rendered book')
-	book = RenderedBook(document, contentLocation)
-	elapsed = time.time() - start_t
-	logger.info("Rendered book created in %s(s)", elapsed)
+    # We very likely will get a book that has no pages
+    # because NTIIDs are not added yet.
+    start_t = time.time()
+    logger.info('Creating rendered book')
+    book = RenderedBook(document, contentLocation)
+    elapsed = time.time() - start_t
+    logger.info("Rendered book created in %s(s)", elapsed)
 
-	# This step adds NTIIDs to the TOC in addition to modifying
-	# on-disk content.
-	logger.info('Adding icons to toc and pages')
-	tociconsetter.transform(book, context=context)
+    # This step adds NTIIDs to the TOC in addition to modifying
+    # on-disk content.
+    logger.info('Adding icons to toc and pages')
+    tociconsetter.transform(book, context=context)
 
-	logger.info('Storing content height in pages')
-	contentsizesetter.transform(book, context=context)
+    logger.info('Storing content height in pages')
+    contentsizesetter.transform(book, context=context)
 
-	logger.info('Adding related links to toc')
-	relatedlinksetter.performTransforms(book, context=context)
+    logger.info('Adding related links to toc')
+    relatedlinksetter.performTransforms(book, context=context)
 
-	# SAJ: Disabled until we determine what thumbnails we need and how to create them
-	# in a useful manner.
-	# logger.info('Generating thumbnails for pages')
-	# contentthumbnails.transform(book, context=context)
+    # SAJ: Disabled until we determine what thumbnails we need and how to create them
+    # in a useful manner.
+    # logger.info('Generating thumbnails for pages')
+    # contentthumbnails.transform(book, context=context)
 
-	# PhantomJS doesn't cope well with the iframes
-	# for embedded videos: you get a black box, and we put them at the top
-	# of the pages, so many thumbnails end up looking the same, and looking
-	# bad. So do this after taking thumbnails.
-	logger.info('Adding videos')
-	sectionvideoadder.performTransforms(book, context=context)
+    # PhantomJS doesn't cope well with the iframes
+    # for embedded videos: you get a black box, and we put them at the top
+    # of the pages, so many thumbnails end up looking the same, and looking
+    # bad. So do this after taking thumbnails.
+    logger.info('Adding videos')
+    sectionvideoadder.performTransforms(book, context=context)
 
-	if dochecking:
-		logger.info('Running checks on content')
-		contentchecks.performChecks(book, context=context)
+    if dochecking:
+        logger.info('Running checks on content')
+        contentchecks.performChecks(book, context=context)
 
-	contentPath = os.path.realpath(contentLocation)
+    contentPath = os.path.realpath(contentLocation)
 
-	# TODO: Aren't the things in the archive mirror file the same things
-	# we want to list in the manifest? If so, we should be able to combine
-	# these steps (if nothing else, just list the contents of the archive to get the
-	# manifest)
-	logger.info("Creating html cache-manifest")
-	html5cachefile.main(contentPath, contentPath)
+    # TODO: Aren't the things in the archive mirror file the same things
+    # we want to list in the manifest? If so, we should be able to combine
+    # these steps (if nothing else, just list the contents of the archive to get the
+    # manifest)
+    logger.info("Creating html cache-manifest")
+    html5cachefile.main(contentPath, contentPath)
 
-	logger.info('Changing intra-content links')
-	ntiidlinksetter.transform(book)
+    logger.info('Changing intra-content links')
+    ntiidlinksetter.transform(book)
 
-	# In case order matters, we sort by the name of the
-	# utility. To register, use patterns like 001, 002, etc.
-	# Ideally order shouldn't matter, and if it does it should be
-	# handled by a specialized dispatching utility.
-	for name, extractor in sorted(component.getUtilitiesFor(IRenderedBookTransformer)):
-		if not IRenderedBookIndexer.providedBy(extractor):
-			logger.info("Extracting %s/%s", name, extractor)
-			extractor.transform(book)
+    # In case order matters, we sort by the name of the
+    # utility. To register, use patterns like 001, 002, etc.
+    # Ideally order shouldn't matter, and if it does it should be
+    # handled by a specialized dispatching utility.
+    for name, extractor in sorted(component.getUtilitiesFor(IRenderedBookTransformer)):
+        if not IRenderedBookIndexer.providedBy(extractor):
+            logger.info("Extracting %s/%s", name, extractor)
+            extractor.transform(book)
 
-	logger.info("Creating JSONP content")
-	jsonpbuilder.transform(book)
+    logger.info("Creating JSONP content")
+    jsonpbuilder.transform(book)
 
-	logger.info("Creating an archive file")
-	archive.create_archive(book, name=jobname)
+    logger.info("Creating an archive file")
+    archive.create_archive(book, name=jobname)
+
 
 def render(document, rname, db):
-	# Apply renderer
-	renderer = createResourceRenderer(rname, db, unmix=False)
-	renderer.render(document)
-	return renderer
+    # Apply renderer
+    renderer = createResourceRenderer(rname, db, unmix=False)
+    renderer.render(document)
+    return renderer
+
 
 def toXml(document, jobname):
-	outfile = '%s.xml' % jobname
-	with open(outfile, 'w') as f:
-		f.write(document.toXML().encode('utf-8'))
+    outfile = '%s.xml' % jobname
+    with open(outfile, 'w') as f:
+        f.write(document.toXML().encode('utf-8'))
+
 
 def write_dc_metadata(document, jobname):
-	"""
-	Write an XML file containing the DublinCore metadata we can extract for this document.
-	"""
-	mapping = {}
-	metadata = document.userdata
+    """
+    Write an XML file containing the DublinCore metadata we can extract for this document.
+    """
+    mapping = {}
+    metadata = document.userdata
 
-	logger.info("Writing DublinCore Metadata.")
+    logger.info("Writing DublinCore Metadata.")
 
-	if 'author' in metadata:
-		# latex author and DC Creator are both arrays
-		mapping['Creator'] = [x.textContent for x in metadata['author']]
+    if 'author' in metadata:
+        # latex author and DC Creator are both arrays
+        mapping['Creator'] = [x.textContent for x in metadata['author']]
 
-	if 'title' in metadata:
-		# DC Title is an array, latex title is scalar
-		# Sometimes title may be a string or it may be a TeXElement, depending
-		# on what packages have dorked things up
-		mapping['Title'] = (getattr(metadata['title'], 'textContent',
-							metadata['title']),)
+    if 'title' in metadata:
+        # DC Title is an array, latex title is scalar
+        # Sometimes title may be a string or it may be a TeXElement, depending
+        # on what packages have dorked things up
+        mapping['Title'] = (getattr(metadata['title'], 'textContent',
+                                    metadata['title']),)
 
-	# The 'date' command in latex is free form, which is not
-	# what we want for DC...what do we want?
+    # The 'date' command in latex is free form, which is not
+    # what we want for DC...what do we want?
 
-	# For other options, see zope.dublincore.dcterms.name_to_element
-	# Publisher, in particular, would be a good one
-	if not mapping:
-		return
+    # For other options, see zope.dublincore.dcterms.name_to_element
+    # Publisher, in particular, would be a good one
+    if not mapping:
+        return
 
-	xml_string = unicode(zope.dublincore.xmlmetadata.dumpString(mapping).decode('utf-8'))
-	with open('dc_metadata.xml', 'w') as f:
-		f.write(xml_string.encode('utf-8'))
+    xml_string = zope.dublincore.xmlmetadata.dumpString(mapping)
+    xml_string = unicode(xml_string.decode('utf-8'))
+    with open('dc_metadata.xml', 'w') as f:
+        f.write(xml_string.encode('utf-8'))
+
 
 def generateImages(document):
-	# Generates required images ###
-	# Replace this with configuration/use of ZCA?
-	OVERRIDE_INDEX_NAME = getattr(ResourceTypeOverrides, 'OVERRIDE_INDEX_NAME')
-	local_overrides = os.path.join(os.getcwd(), '../nti.resourceoverrides')
-	if os.path.exists(os.path.join(local_overrides, OVERRIDE_INDEX_NAME)):
-		overrides = local_overrides
-	else:
-		overrides = resource_filename(__name__, 'resourceoverrides')
-	db = ResourceDB(document, overridesLocation=overrides)
-	db.generateResourceSets()
-	return db
+    # Generates required images ###
+    # Replace this with configuration/use of ZCA?
+    OVERRIDE_INDEX_NAME = getattr(ResourceTypeOverrides, 'OVERRIDE_INDEX_NAME')
+    local_overrides = os.path.join(os.getcwd(), '../nti.resourceoverrides')
+    if os.path.exists(os.path.join(local_overrides, OVERRIDE_INDEX_NAME)):
+        overrides = local_overrides
+    else:
+        overrides = resource_filename(__name__, 'resourceoverrides')
+    db = ResourceDB(document, overridesLocation=overrides)
+    db.generateResourceSets()
+    return db
