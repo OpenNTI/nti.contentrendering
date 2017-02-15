34573c8aa52f4c975d8167dae337527c06eda216
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-02-15 16:11:04 +0000

    minidom only makes sense in nti.contentrendering
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentrendering/trunk@106618 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/utils/__init__.py
+++ src/nti/contentrendering/utils/__init__.py
@@ -10,46 +10,10 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-from nti.contentrendering.RenderedBook import RenderedBook
-
 from nti.contentrendering.utils.chameleon import setupChameleonCache
 
-class EmptyMockDocument(object):
+from nti.contentrendering.utils.rendered_book import EmptyMockDocument
+from nti.contentrendering.utils.rendered_book import NoPhantomRenderedBook
+from nti.contentrendering.utils.rendered_book import NoConcurrentPhantomRenderedBook
 
-    childNodes = ()
-
-    def __init__(self):
-        self.context = {}
-        self.userdata = {}
-
-    def getElementsByTagName(self, name):
-        return ()
-
-
-def _phantom_function(htmlFile, scriptName, args, key):
-    return (key, {'ntiid': key[0]})
-
-
-class NoPhantomRenderedBook(RenderedBook):
-
-    def _get_phantom_function(self):
-        return _phantom_function
-
-
-class NoConcurrentPhantomRenderedBook(RenderedBook):
-
-    # Replaces RenderedBook.runPhandomOnPages with a version that does not require
-    # phantomJS and does not  call the sometimes problematic concurrency module.
-    # Useful for the creation of RenderedBook objects where we are only going to alter
-    # basic things such as the index or sharewith attribute.
-    def runPhantomOnPages(self, script, *args):
-        results = {}
-        eclipseTOC = self.toc
-        nodesForPages = eclipseTOC.getPageNodes()
-        for node in nodesForPages:
-            key = (node.getAttribute(b'ntiid'),
-                   node.getAttribute(b'href'),
-                   node.getAttribute(b'label'))
-            result = {'ntiid': key[0]}
-            results[key] = result
-        return results
+from nti.contentrendering.utils.rendered_book import _phantom_function
---
+++ src/nti/contentrendering/utils/minidom.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+"""
+Utilities for working with :mod:`xml.dom.minidom` objects.
+
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+import io
+
+
+def minidom_writexml(document, outfile, encoding=u'utf-8'):
+    """
+    Papers over some very bad Unicode issues
+    that crop up with xml.dom.minidom.
+    """
+    class _StupidIOPackageCannotDealWithBothStrAndUnicodeObjects(object):
+
+        def __init__(self, under):
+            self._under = under
+
+        def write(self, x):
+            if isinstance(x, str):
+                x = unicode(x)
+            self._under.write(x)
+
+    with io.open(outfile, "w", encoding=encoding) as f:
+        document.writexml(_StupidIOPackageCannotDealWithBothStrAndUnicodeObjects(f),
+                          encoding=encoding)
