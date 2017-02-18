036c498ac5227f794c863f75d5a6b1033d5cd3de
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-02-18 18:38:07 +0000

    pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentrendering/trunk@106926 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/__init__.py
+++ src/nti/contentrendering/__init__.py
@@ -23,97 +23,102 @@ from nti.contentrendering.phantom import _PhantomProducedUnexpectedOutputError
 from nti.futures.futures import ConcurrentExecutor
 ConcurrentExecutor = ConcurrentExecutor
 
+
 class _NotFound(object):
-	"""
-	A descriptive False object
-	"""
+    """
+    A descriptive False object
+    """
 
-	def __init__(self, msg):
-		self.msg = msg
+    def __init__(self, msg):
+        self.msg = msg
 
-	def __nonzero__(self):
-		return False
-	__bool__ = __nonzero__
+    def __nonzero__(self):
+        return False
+    __bool__ = __nonzero__
 
-	def __str__(self):
-		return self.msg
+    def __str__(self):
+        return self.msg
+
 
 class _programproperty(object):
-	"""
-	A readproperty (meaning you can override in the instance)
-	that knows how to hold verification arguments.
-	"""
+    """
+    A readproperty (meaning you can override in the instance)
+    that knows how to hold verification arguments.
+    """
 
-	def __init__(self, env_name, prog_name, verify_arg=''):
-		self.env_name = env_name
-		self.prog_name = prog_name
-		self.verify_arg = verify_arg
+    def __init__(self, env_name, prog_name, verify_arg=''):
+        self.env_name = env_name
+        self.prog_name = prog_name
+        self.verify_arg = verify_arg
 
-	def __get__(self, inst, klass):
-		if inst is None:
-			return self
-		return os.environ.get(self.env_name, self.prog_name)
+    def __get__(self, inst, klass):
+        if inst is None:
+            return self
+        return os.environ.get(self.env_name, self.prog_name)
+
 
 class _ExternalProgramSettings(object):
-	"""
-	Provisional API to standardize the way we find and run
-	external programs.
-	"""
+    """
+    Provisional API to standardize the way we find and run
+    external programs.
+    """
 
-	# Lacking a clean way to register new programs to verify
+    # Lacking a clean way to register new programs to verify
 
-	def __init__(self):
-		self._verify_cache = {}
+    def __init__(self):
+        self._verify_cache = {}
 
-	gs = _programproperty('GHOSTSCRIPT', 'gs', '--help')
-	# Identify is provided by ImageMagick
-	identify = _programproperty('IDENTIFY', 'identify')
-	# convert is provided by ImageMagick
-	convert = _programproperty('CONVERT', 'convert')
-	pngcrush = _programproperty('PNGCRUSH', 'pngcrush')
-	pdfcrop = _programproperty("PDFCROP", 'pdfcrop')
-	pdf2svg = _programproperty("PDF2SVG", 'pdf2svg')
+    gs = _programproperty('GHOSTSCRIPT', 'gs', '--help')
+    # Identify is provided by ImageMagick
+    identify = _programproperty('IDENTIFY', 'identify')
+    # convert is provided by ImageMagick
+    convert = _programproperty('CONVERT', 'convert')
+    pngcrush = _programproperty('PNGCRUSH', 'pngcrush')
+    pdfcrop = _programproperty("PDFCROP", 'pdfcrop')
+    pdf2svg = _programproperty("PDF2SVG", 'pdf2svg')
 
-	def verify(self, programs=None):
-		"""
-		Check that all the needed programs are on the PATH and executable.
+    def verify(self, programs=None):
+        """
+        Check that all the needed programs are on the PATH and executable.
 
-		:param programs: If given, a sequence of the program names to check for.
-		"""
-		if programs is not None and isinstance(programs, six.string_types):
-			programs = (programs,)
+        :param programs: If given, a sequence of the program names to check for.
+        """
+        if programs is not None and isinstance(programs, six.string_types):
+            programs = (programs,)
 
-		for k, v in self.__class__.__dict__.items():
-			if not isinstance(v, _programproperty):
-				continue
-			if programs is not None and k not in programs:
-				continue
+        for k, v in self.__class__.__dict__.items():
+            if not isinstance(v, _programproperty):
+                continue
+            if programs is not None and k not in programs:
+                continue
 
-			program = getattr(self, k)
-			if k in self._verify_cache and self._verify_cache[k][0] == program:
-				if not self._verify_cache[k][1]:
-					return self._verify_cache[k][1]
-				continue
+            program = getattr(self, k)
+            if k in self._verify_cache and self._verify_cache[k][0] == program:
+                if not self._verify_cache[k][1]:
+                    return self._verify_cache[k][1]
+                continue
 
-			__traceback_info__ = k, program
-			try:
-				command = [program, v.verify_arg] if v.verify_arg else program
-				proc = subprocess.Popen(command,
-										shell=False,
-										stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
-										close_fds=True)
-				output, _ = proc.communicate()
-			except OSError:
-				output = None
+            __traceback_info__ = k, program
+            try:
+                command = [program, v.verify_arg] if v.verify_arg else program
+                proc = subprocess.Popen(command,
+                                        shell=False,
+                                        stdout=subprocess.PIPE,
+                                        stderr=subprocess.STDOUT,
+                                        close_fds=True)
+                output, _ = proc.communicate()
+            except OSError:
+                output = None
 
-			found = bool(output)
-			if not found:
-				found = _NotFound("Unable to find program %s for property %s" % (program, k))
+            found = bool(output)
+            if not found:
+                found = _NotFound("Unable to find program %s for property %s" %
+                                  (program, k))
 
-			self._verify_cache[k] = program, found
-			if not found:
-				logger.warn(found)
-				return found
-		return True
+            self._verify_cache[k] = program, found
+            if not found:
+                logger.warn(found)
+                return found
+        return True
 
 _programs = _ExternalProgramSettings()
