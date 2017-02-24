22721e6760a02f0400df5725679ec6404992868d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-02-24 17:32:12 +0000

    add ntilists
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentrendering/trunk@107402 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contentrendering/plastexpackages/ntilists.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from plasTeX.Base.LaTeX.Lists import List
+from plasTeX.Base.LaTeX.Lists import itemize
+from plasTeX.Base.LaTeX.Lists import trivlist
+from plasTeX.Base.LaTeX.Lists import enumerate_
+
+
+class ntilist(List):
+    pass
+
+
+class ntiitemize(itemize):
+    pass
+
+
+class ntienumerate(enumerate_):
+    pass
+
+
+class ntitrivlist(trivlist):
+    pass
