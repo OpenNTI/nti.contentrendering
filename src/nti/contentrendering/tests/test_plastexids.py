ead772533174a9c54d87d68711ebb67fd4e4bede
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-02 17:45:01 +0000

    fix tests
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentrendering/trunk@107844 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/tests/test_plastexids.py
+++ src/nti/contentrendering/tests/test_plastexids.py
@@ -7,76 +7,78 @@ __docformat__ = "restructuredtext en"
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
 
-import unittest
-
-from hamcrest import assert_that
 from hamcrest import is_
-from hamcrest import is_not as does_not
 from hamcrest import has_entry
+from hamcrest import assert_that
 from hamcrest import has_property
 from hamcrest import contains_string
+from hamcrest import is_not as does_not
 
-from nti.contentrendering.tests import setChameleonCache
-from nti.contentrendering.tests import restoreChameleonCache
+import unittest
 
-from nti.contentrendering.tests import buildDomFromString
-from nti.contentrendering.tests import simpleLatexDocumentText
+from plasTeX.Context import Context
 
 from nti.contentrendering.plastexids import patch_all
 from nti.contentrendering.plastexids import _section_ntiid
 from nti.contentrendering.plastexids import _section_ntiid_filename
 
-from plasTeX.Context import Context
+from nti.contentrendering.tests import buildDomFromString
+from nti.contentrendering.tests import simpleLatexDocumentText
+
 
 class IdPatchedLayer(object):
 
-	@classmethod
-	def setUp(cls):
-		setChameleonCache(cls)
-		patch_all() # sadly, this is not reversible
+    @classmethod
+    def setUp(cls):
+        patch_all()  # sadly, this is not reversible
 
-	@classmethod
-	def tearDown(cls):
-		pass
+    @classmethod
+    def tearDown(cls):
+        pass
 
-	@classmethod
-	def testTearDown(cls):
-		restoreChameleonCache(cls)
+    @classmethod
+    def testTearDown(cls):
+        pass
 
-	setUpTest = tearDownTest = tearDown
+    setUpTest = tearDownTest = tearDown
 
 # For non-layer aware runners...
 # this isn't reversible anyway
 patch_all()
 
+
 class TestPlastexIds(unittest.TestCase):
 
-	layer = IdPatchedLayer
+    layer = IdPatchedLayer
 
-	def test_escape_filename(self):
-		dom = buildDomFromString( simpleLatexDocumentText(bodies=(r'\chapter{A & () : Chapter}',) ) )
+    def test_escape_filename(self):
+        dom = buildDomFromString(
+            simpleLatexDocumentText(bodies=(r'\chapter{A & () : Chapter}',)))
 
-		chapter = dom.getElementsByTagName( 'chapter' )[0]
+        chapter = dom.getElementsByTagName('chapter')[0]
 
-		assert_that( _section_ntiid_filename( chapter ),
-					 is_( 'tag_nextthought_com_2011-10_testing-HTML-temp_a_______chapter' ) )
+        assert_that(_section_ntiid_filename(chapter),
+                    is_('tag_nextthought_com_2011-10_testing-HTML-temp_a_______chapter'))
 
-	def test_case_insensitive_ntiids_collision(self):
-		dom = buildDomFromString( simpleLatexDocumentText(bodies=(r'\chapter{A}\chapter{a}',) ) )
+    def test_case_insensitive_ntiids_collision(self):
+        dom = buildDomFromString(
+            simpleLatexDocumentText(bodies=(r'\chapter{A}\chapter{a}',)))
 
-		chapter1 = dom.getElementsByTagName( 'chapter' )[0]
-		chapter2 = dom.getElementsByTagName( 'chapter' )[1]
+        chapter1 = dom.getElementsByTagName('chapter')[0]
+        chapter2 = dom.getElementsByTagName('chapter')[1]
 
+        assert_that(_section_ntiid(chapter1), is_(
+            'tag:nextthought.com,2011-10:testing-HTML-temp.a'))
+        assert_that(_section_ntiid(chapter2), is_(
+            'tag:nextthought.com,2011-10:testing-HTML-temp.a.1'))
 
-		assert_that( _section_ntiid( chapter1 ), is_(  'tag:nextthought.com,2011-10:testing-HTML-temp.a' ) )
-		assert_that( _section_ntiid( chapter2 ), is_( 'tag:nextthought.com,2011-10:testing-HTML-temp.a.1' ) )
+        assert_that(_section_ntiid_filename(chapter1), is_(
+            'tag_nextthought_com_2011-10_testing-HTML-temp_a'))
+        assert_that(_section_ntiid_filename(chapter2), is_(
+            'tag_nextthought_com_2011-10_testing-HTML-temp_a_1'))
 
-		assert_that( _section_ntiid_filename( chapter1 ), is_(  'tag_nextthought_com_2011-10_testing-HTML-temp_a' ) )
-		assert_that( _section_ntiid_filename( chapter2 ), is_( 'tag_nextthought_com_2011-10_testing-HTML-temp_a_1' ) )
-
-
-	def test_cross_doc_refs(self):
-		dom_str = r'''
+    def test_cross_doc_refs(self):
+        dom_str = r'''
 		\chapter{A}
 		\label{A}
 		Some text
@@ -84,33 +86,36 @@ class TestPlastexIds(unittest.TestCase):
 		\label{B}
 		Some other text
 		'''
-		dom = buildDomFromString( simpleLatexDocumentText(bodies=(dom_str,) ) )
+        dom = buildDomFromString(simpleLatexDocumentText(bodies=(dom_str,)))
 
-		chapter1 = dom.getElementsByTagName( 'chapter' )[0]
-		chapter2 = dom.getElementsByTagName( 'chapter' )[1]
+        chapter1 = dom.getElementsByTagName('chapter')[0]
+        chapter2 = dom.getElementsByTagName('chapter')[1]
 
-		chapter1_ntiid = chapter1.ntiid
-		chapter2_ntiid = chapter2.ntiid
+        chapter1_ntiid = chapter1.ntiid
+        chapter2_ntiid = chapter2.ntiid
 
-		bytes_io = dom.context.persist(None)
+        bytes_io = dom.context.persist(None)
 
-		context = Context()
-		bytes_io.seek(0)
+        context = Context()
+        bytes_io.seek(0)
 
-		context.restore(bytes_io)
+        context.restore(bytes_io)
 
-		assert_that(context.labels, has_entry( 'A', has_property('ntiid', chapter1_ntiid) ))
-		assert_that(context.labels, has_entry( 'B', has_property('ntiid', chapter2_ntiid) ))
+        assert_that(context.labels, has_entry(
+            'A', has_property('ntiid', chapter1_ntiid)))
+        assert_that(context.labels, has_entry(
+            'B', has_property('ntiid', chapter2_ntiid)))
 
-from nti.contentrendering.tests import ContentrenderingLayerTest
-from nti.contentrendering.tests import RenderContext
-import os
 import io
+import os
+
+from nti.contentrendering.tests import RenderContext
+from nti.contentrendering.tests import ContentrenderingLayerTest
 
 class TestPlastexIdsRender(ContentrenderingLayerTest):
 
-	def test_cross_doc_ref_links(self):
-		source_str = r'''
+    def test_cross_doc_ref_links(self):
+        source_str = r'''
 		\chapter{A}
 		\label{A}
 		Some text
@@ -143,7 +148,7 @@ class TestPlastexIdsRender(ContentrenderingLayerTest):
 		We would like to ref the equation in \ref{figure}.
 		'''
 
-		ref_str = r'''
+        ref_str = r'''
 		\chapter{One}
 		\label{One}
 		Back ref to chapter \ntiidref{A}.
@@ -160,68 +165,74 @@ class TestPlastexIdsRender(ContentrenderingLayerTest):
 
 		'''
 
-		source_a_ntiid = "tag:nextthought.com,2011-10:testing-HTML-temp.A"
-		source_b_ntiid = "tag:nextthought.com,2011-10:testing-HTML-temp.SectionB"
+        source_a_ntiid = "tag:nextthought.com,2011-10:testing-HTML-temp.A"
+        source_b_ntiid = "tag:nextthought.com,2011-10:testing-HTML-temp.SectionB"
 
+        with RenderContext(simpleLatexDocumentText(preludes=(br'\usepackage{nti.contentrendering.plastexpackages.ntilatexmacros}',
+                                                             br'\usepackage{amsmath}'),
+                                                   bodies=(source_str,))) as source_context:
 
-		with RenderContext(simpleLatexDocumentText( preludes=(br'\usepackage{nti.contentrendering.plastexpackages.ntilatexmacros}',
-															  br'\usepackage{amsmath}'),
-													bodies=(source_str,) )) as source_context:
+            source_context.render()
 
-			source_context.render()
+            # Our ref should have rendered as a file
+            chapter_b = source_context.read_rendered_file(
+                'tag_nextthought_com_2011-10_testing-HTML-temp_B.html')
+            assert_that(chapter_b,
+                        contains_string('href="tag_nextthought_com_2011-10_testing-HTML-temp_A.html"'))
 
-			# Our ref should have rendered as a file
-			chapter_b = source_context.read_rendered_file('tag_nextthought_com_2011-10_testing-HTML-temp_B.html')
-			assert_that( chapter_b,
-						 contains_string('href="tag_nextthought_com_2011-10_testing-HTML-temp_A.html"'))
+            sec_b = source_context.read_rendered_file(
+                'tag_nextthought_com_2011-10_testing-HTML-temp_SectionB.html')
+            # check the back-ref to the subsection
+            assert_that(sec_b,
+                        contains_string('tag_nextthought_com_2011-10_testing-HTML-temp_A.html#SubsectionA'))
 
-			sec_b = source_context.read_rendered_file('tag_nextthought_com_2011-10_testing-HTML-temp_SectionB.html')
-			# check the back-ref to the subsection
-			assert_that( sec_b,
-						 contains_string('tag_nextthought_com_2011-10_testing-HTML-temp_A.html#SubsectionA'))
+            # check the original equation
+            assert_that(sec_b,
+                        contains_string('id="equation"'))
+            # check its reference.
+            assert_that(sec_b,
+                        contains_string('href="tag_nextthought_com_2011-10_testing-HTML-temp_SectionB.html#equation"'))
 
-			# check the original equation
-			assert_that( sec_b,
-						 contains_string('id="equation"'))
-			# check its reference.
-			assert_that( sec_b,
-						 contains_string('href="tag_nextthought_com_2011-10_testing-HTML-temp_SectionB.html#equation"'))
+            # The ntiid for the equation did not make it into the ToC
+            eqn_ntiid = source_context.dom.getElementsByTagName(
+                'equation')[0].ntiid
+            fig_ntiid = source_context.dom.getElementsByTagName(
+                'figure')[0].getElementsByTagName('caption')[0].ntiid
+            assert_that(eqn_ntiid, is_(
+                'tag:nextthought.com,2011-10:testing-HTMLequation-temp.equation.equation'))
+            assert_that(fig_ntiid, is_(
+                'tag:nextthought.com,2011-10:testing-HTMLfigure-temp.figure.figure'))
+            toc = source_context.read_rendered_file('eclipse-toc.xml')
+            for i in eqn_ntiid, fig_ntiid:
+                assert_that(toc,
+                            does_not(contains_string(i)))
 
-			# The ntiid for the equation did not make it into the ToC
-			eqn_ntiid = source_context.dom.getElementsByTagName('equation')[0].ntiid
-			fig_ntiid = source_context.dom.getElementsByTagName('figure')[0].getElementsByTagName('caption')[0].ntiid
-			assert_that( eqn_ntiid, is_('tag:nextthought.com,2011-10:testing-HTMLequation-temp.equation.equation'))
-			assert_that( fig_ntiid, is_('tag:nextthought.com,2011-10:testing-HTMLfigure-temp.figure.figure'))
-			toc = source_context.read_rendered_file('eclipse-toc.xml')
-			for i in eqn_ntiid, fig_ntiid:
-				assert_that( toc,
-							 does_not( contains_string(i)))
+            # Capture the value of the paux file for use later, so
+            # we can let this render be completely cleaned up
+            with open(os.path.join(source_context.docdir, 'temp.paux'), 'rb') as f:
+                paux_value = f.read()
 
-			# Capture the value of the paux file for use later, so
-			# we can let this render be completely cleaned up
-			with open(os.path.join(source_context.docdir, 'temp.paux'), 'rb') as f:
-				paux_value = f.read()
+        def load_links(document):
+            paux_io = io.BytesIO(paux_value)
+            document.context.restore(paux_io,
+                                     rtype='XHTML')
 
-		def load_links(document):
-			paux_io = io.BytesIO(paux_value)
-			document.context.restore(paux_io,
-									 rtype='XHTML')
+        with RenderContext(simpleLatexDocumentText(preludes=(br'\usepackage{nti.contentrendering.plastexpackages.ntilatexmacros}',),
+                                                   bodies=(ref_str,)),
+                           packages_on_texinputs=True,
+                           config_hook=load_links) as ref_context:
+            ref_context.render()
 
+            chapter_one = ref_context.read_rendered_file(
+                'tag_nextthought_com_2011-10_testing-HTML-temp_One.html')
+            section_two = ref_context.read_rendered_file(
+                'tag_nextthought_com_2011-10_testing-HTML-temp_Two.html')
 
-		with RenderContext(simpleLatexDocumentText( preludes=(br'\usepackage{nti.contentrendering.plastexpackages.ntilatexmacros}',),
-													bodies=(ref_str,)),
-						   packages_on_texinputs=True,
-						   config_hook=load_links) as ref_context:
-			ref_context.render()
+            assert_that(chapter_one,
+                        contains_string('href="%s"' % source_a_ntiid))
 
-			chapter_one = ref_context.read_rendered_file('tag_nextthought_com_2011-10_testing-HTML-temp_One.html')
-			section_two = ref_context.read_rendered_file('tag_nextthought_com_2011-10_testing-HTML-temp_Two.html')
-
-			assert_that( chapter_one,
-						 contains_string('href="%s"' % source_a_ntiid))
-
-			# Note that the embedded things are referenced
-			# by DOM ID relative to the containing ntiid
-			for ntiid in source_b_ntiid, source_b_ntiid + '#equation', source_b_ntiid + '#figure', source_a_ntiid + '#SubsectionA':
-				assert_that( section_two,
-							 contains_string('href="%s"' % ntiid))
+            # Note that the embedded things are referenced
+            # by DOM ID relative to the containing ntiid
+            for ntiid in source_b_ntiid, source_b_ntiid + '#equation', source_b_ntiid + '#figure', source_a_ntiid + '#SubsectionA':
+                assert_that(section_two,
+                            contains_string('href="%s"' % ntiid))
