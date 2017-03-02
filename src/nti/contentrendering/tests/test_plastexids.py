83bc79edb4368475ab74e863af63e250f320444d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-02 17:57:34 +0000

    pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentrendering/trunk@107845 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/tests/test_plastexids.py
+++ src/nti/contentrendering/tests/test_plastexids.py
@@ -79,13 +79,13 @@ class TestPlastexIds(unittest.TestCase):
 
     def test_cross_doc_refs(self):
         dom_str = r'''
-		\chapter{A}
-		\label{A}
-		Some text
-		\chapter{B}
-		\label{B}
-		Some other text
-		'''
+        \chapter{A}
+        \label{A}
+        Some text
+        \chapter{B}
+        \label{B}
+        Some other text
+        '''
         dom = buildDomFromString(simpleLatexDocumentText(bodies=(dom_str,)))
 
         chapter1 = dom.getElementsByTagName('chapter')[0]
@@ -101,10 +101,10 @@ class TestPlastexIds(unittest.TestCase):
 
         context.restore(bytes_io)
 
-        assert_that(context.labels, has_entry(
-            'A', has_property('ntiid', chapter1_ntiid)))
-        assert_that(context.labels, has_entry(
-            'B', has_property('ntiid', chapter2_ntiid)))
+        assert_that(context.labels, 
+                    has_entry('A', has_property('ntiid', chapter1_ntiid)))
+        assert_that(context.labels, 
+                    has_entry('B', has_property('ntiid', chapter2_ntiid)))
 
 import io
 import os
@@ -116,54 +116,54 @@ class TestPlastexIdsRender(ContentrenderingLayerTest):
 
     def test_cross_doc_ref_links(self):
         source_str = r'''
-		\chapter{A}
-		\label{A}
-		Some text
+        \chapter{A}
+        \label{A}
+        Some text
 
-		\subsection{SubsectionA}
-		\label{SubsectionA}
-		This is in the same HTML page as the chapter.
+        \subsection{SubsectionA}
+        \label{SubsectionA}
+        This is in the same HTML page as the chapter.
 
-		\chapter{B}
-		\label{B}
-		Some other text \ref{A}.
+        \chapter{B}
+        \label{B}
+        Some other text \ref{A}.
 
-		\section{SectionB}
-		\label{SectionB}
-		Some text in subsection that refs \ref{SubsectionA}.
+        \section{SectionB}
+        \label{SectionB}
+        Some text in subsection that refs \ref{SubsectionA}.
 
-		\begin{equation}
-		\label{equation}
-			\forall x \in X, \quad \exists y \leq \epsilon
-		\end{equation}
+        \begin{equation}
+        \label{equation}
+            \forall x \in X, \quad \exists y \leq \epsilon
+        \end{equation}
 
-		We would like to ref the equation in \ref{equation}.
+        We would like to ref the equation in \ref{equation}.
 
-		\begin{figure}
-		\caption{A figure}
-		\label{figure} % recall that captions are labeled
-		Stuff in figure
-		\end{figure}
+        \begin{figure}
+        \caption{A figure}
+        \label{figure} % recall that captions are labeled
+        Stuff in figure
+        \end{figure}
 
-		We would like to ref the equation in \ref{figure}.
-		'''
+        We would like to ref the equation in \ref{figure}.
+        '''
 
         ref_str = r'''
-		\chapter{One}
-		\label{One}
-		Back ref to chapter \ntiidref{A}.
+        \chapter{One}
+        \label{One}
+        Back ref to chapter \ntiidref{A}.
 
 
-		\section{Two}
-		Section refs \ntiidref{SectionB}.
+        \section{Two}
+        Section refs \ntiidref{SectionB}.
 
-		Section also refs \ntiidref{equation}.
+        Section also refs \ntiidref{equation}.
 
-		Section also refs \ntiidref{figure}.
+        Section also refs \ntiidref{figure}.
 
-		Back ref to subsection \ntiidref{SubsectionA}.
+        Back ref to subsection \ntiidref{SubsectionA}.
 
-		'''
+        '''
 
         source_a_ntiid = "tag:nextthought.com,2011-10:testing-HTML-temp.A"
         source_b_ntiid = "tag:nextthought.com,2011-10:testing-HTML-temp.SectionB"
@@ -194,14 +194,16 @@ class TestPlastexIdsRender(ContentrenderingLayerTest):
                         contains_string('href="tag_nextthought_com_2011-10_testing-HTML-temp_SectionB.html#equation"'))
 
             # The ntiid for the equation did not make it into the ToC
-            eqn_ntiid = source_context.dom.getElementsByTagName(
-                'equation')[0].ntiid
-            fig_ntiid = source_context.dom.getElementsByTagName(
-                'figure')[0].getElementsByTagName('caption')[0].ntiid
-            assert_that(eqn_ntiid, is_(
-                'tag:nextthought.com,2011-10:testing-HTMLequation-temp.equation.equation'))
-            assert_that(fig_ntiid, is_(
-                'tag:nextthought.com,2011-10:testing-HTMLfigure-temp.figure.figure'))
+            equation = source_context.dom.getElementsByTagName('equation')[0]
+            eqn_ntiid = equation.ntiid
+            
+            figure = source_context.dom.getElementsByTagName('figure')[0]
+            fig_ntiid = figure.getElementsByTagName('caption')[0].ntiid
+            assert_that(eqn_ntiid, 
+                        is_('tag:nextthought.com,2011-10:testing-HTMLequation-temp.equation.equation'))
+            assert_that(fig_ntiid, 
+                        is_('tag:nextthought.com,2011-10:testing-HTMLfigure-temp.figure.figure'))
+
             toc = source_context.read_rendered_file('eclipse-toc.xml')
             for i in eqn_ntiid, fig_ntiid:
                 assert_that(toc,
@@ -233,6 +235,9 @@ class TestPlastexIdsRender(ContentrenderingLayerTest):
 
             # Note that the embedded things are referenced
             # by DOM ID relative to the containing ntiid
-            for ntiid in source_b_ntiid, source_b_ntiid + '#equation', source_b_ntiid + '#figure', source_a_ntiid + '#SubsectionA':
+            for ntiid in (source_b_ntiid,
+                          source_b_ntiid + '#equation', 
+                          source_b_ntiid + '#figure', 
+                          source_a_ntiid + '#SubsectionA'):
                 assert_that(section_two,
                             contains_string('href="%s"' % ntiid))
