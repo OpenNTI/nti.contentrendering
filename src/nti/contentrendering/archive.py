90625fbefa68860c2c794c1fa7c7c8d83aa2a2f1
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-02 00:29:27 +0000

    pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentrendering/trunk@107799 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/archive.py
+++ src/nti/contentrendering/archive.py
@@ -18,7 +18,7 @@ import zipfile
 import argparse
 
 from zope import component
-from zope import interface
+
 
 from zope.configuration import config
 from zope.configuration import xmlconfig
@@ -31,97 +31,105 @@ from nti.contentrendering.utils import EmptyMockDocument
 from nti.contentrendering.utils import NoConcurrentPhantomRenderedBook
 
 BLACK_LIST = (r'.*\.jsonp', r'indexdir', r'cache-manifest', r'archive\.zip',
-			  r'htaccess', r'assessment_index\.json', r'\.nti_acl', r'\.htaccess')
-BLACK_LIST_RE = tuple([re.compile(x) for x in BLACK_LIST])
+              r'htaccess', r'assessment_index\.json', r'\.nti_acl', r'\.htaccess')
+BLACK_LIST_RE = tuple(re.compile(x) for x in BLACK_LIST)
 
-interface.moduleProvides(IRenderedBookArchiver)
 
 def is_black_listed(name):
-	for pattern in BLACK_LIST_RE:
-		if pattern.match(name):
-			return True
-	return False
+    for pattern in BLACK_LIST_RE:
+        if pattern.match(name):
+            return True
+    return False
+
 
 def _archive(source_path, out_dir=None, verbose=False):
-	source_path = os.path.expanduser(source_path)
-	source_path = source_path + '/' if not source_path.endswith('/') else source_path
-	if not os.path.exists(source_path) or not os.path.isdir(source_path):
-		raise Exception("Invalid source directory")
+    source_path = os.path.expanduser(source_path)
+    if not source_path.endswith('/'):
+        source_path += '/'
+    if not os.path.exists(source_path) or not os.path.isdir(source_path):
+        raise IOError("Invalid source directory")
 
-	added = set()
-	out_dir = out_dir or source_path
-	out_dir = os.path.expanduser(out_dir)
-	if not os.path.exists(out_dir):
-		os.makedirs(out_dir)
+    added = set()
+    out_dir = out_dir or source_path
+    out_dir = os.path.expanduser(out_dir)
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
 
-	outfile = os.path.join(out_dir, 'archive.zip')
-	logger.info("Archiving '%s' to '%s'", source_path, outfile)
+    outfile = os.path.join(out_dir, 'archive.zip')
+    logger.info("Archiving '%s' to '%s'", source_path, outfile)
 
-	zf = zipfile.ZipFile(outfile, mode="w")
-	try:
-		os.chdir(source_path)
-		dname = os.path.basename(source_path[0:-1])
+    zf = zipfile.ZipFile(outfile, mode="w")
+    try:
+        os.chdir(source_path)
+        dname = os.path.basename(source_path[0:-1])
 
-		def _add_2_archive(pattern):
-			for pathname in glob.glob(pattern):
-				name = os.path.basename(pathname)
-				if is_black_listed(name):
-					continue
-				if os.path.isdir(pathname):
-					_process(pathname)
-				else:
-					arcname = dname + '/' + pathname[len(source_path):]
-					added.add(arcname)
-					if verbose:
-						logger.info("Adding %s" % arcname)
-					zf.write(pathname, arcname=arcname,
-							 compress_type=zipfile.ZIP_DEFLATED)
+        def _add_2_archive(pattern):
+            for pathname in glob.glob(pattern):
+                name = os.path.basename(pathname)
+                if is_black_listed(name):
+                    continue
+                if os.path.isdir(pathname):
+                    _process(pathname)
+                else:
+                    arcname = dname + '/' + pathname[len(source_path):]
+                    added.add(arcname)
+                    if verbose:
+                        logger.info("Adding %s" % arcname)
+                    zf.write(pathname, arcname=arcname,
+                             compress_type=zipfile.ZIP_DEFLATED)
 
-		def _process(path):
-			_add_2_archive(os.path.join(path, "*"))
-			_add_2_archive(os.path.join(path, ".*"))
+        def _process(path):
+            _add_2_archive(os.path.join(path, "*"))
+            _add_2_archive(os.path.join(path, ".*"))
 
-		_process(source_path)
-	finally:
-		zf.close()
+        _process(source_path)
+    finally:
+        zf.close()
 
-	return added
+    return added
+
 
 def archive(book, out_dir=None, verbose=False):
-	location = os.path.expanduser(book.contentLocation)
-	return _archive(location, out_dir, verbose)
+    location = os.path.expanduser(book.contentLocation)
+    return _archive(location, out_dir, verbose)
+
 
 def create_archive(book, out_dir=None, verbose=False, name=u''):
-	archiver = component.queryUtility(IRenderedBookArchiver, name=name)
-	if archiver is None:
-		archiver = component.queryUtility(IRenderedBookArchiver)
-	result = archiver.archive(book, out_dir, verbose)
-	return result
+    archiver = component.queryUtility(IRenderedBookArchiver, name=name)
+    if archiver is None:
+        archiver = component.queryUtility(IRenderedBookArchiver)
+    result = archiver.archive(book, out_dir, verbose)
+    return result
+
 
 def main():
-	context = config.ConfigurationMachine()
-	xmlconfig.registerCommonDirectives(context)
-	xmlconfig.file("configure.zcml", nti.contentrendering, context=context)
+    context = config.ConfigurationMachine()
+    xmlconfig.registerCommonDirectives(context)
+    xmlconfig.file("configure.zcml", nti.contentrendering, context=context)
 
-	arg_parser = argparse.ArgumentParser(description="Archive book content")
-	arg_parser.add_argument('content_path', help="Book content path")
-	arg_parser.add_argument("-o", "--outdir", dest='out_dir', help="Output directory")
-	arg_parser.add_argument('-a', '--archiver', dest='archiver',
-							help="The archiver name")
-	arg_parser.add_argument('-v', '--verbose', help="Verbose output",
-							action='store_true', dest='verbose')
-	args = arg_parser.parse_args()
+    arg_parser = argparse.ArgumentParser(description="Archive book content")
+    arg_parser.add_argument('content_path', help="Book content path")
+    arg_parser.add_argument( "-o", "--outdir", 
+                            dest='out_dir', help="Output directory")
+    arg_parser.add_argument('-a', '--archiver', dest='archiver',
+                            help="The archiver name")
+    arg_parser.add_argument('-v', '--verbose', help="Verbose output",
+                            action='store_true', dest='verbose')
+    args = arg_parser.parse_args()
 
-	verbose = args.verbose
-	archiver = args.archiver or u''
-	content_path = os.path.expanduser(args.content_path)
-	jobname = os.path.basename(content_path)
-	out_dir = args.out_dir or content_path
+    verbose = args.verbose
+    archiver = args.archiver or u''
+    content_path = os.path.expanduser(args.content_path)
+    jobname = os.path.basename(content_path)
+    out_dir = args.out_dir or content_path
 
-	document = EmptyMockDocument()
-	document.userdata['jobname'] = jobname
-	book = NoConcurrentPhantomRenderedBook(document, content_path)
-	create_archive(book, out_dir, verbose, archiver)
+    document = EmptyMockDocument()
+    document.userdata['jobname'] = jobname
+    book = NoConcurrentPhantomRenderedBook(document, content_path)
+    create_archive(book, out_dir, verbose, archiver)
 
 if __name__ == '__main__':
-	main()
+    main()
+
+from zope import interface
+interface.moduleProvides(IRenderedBookArchiver)
--- src/nti/contentrendering/tests/test_archive.py
+++ src/nti/contentrendering/tests/test_archive.py
@@ -11,11 +11,11 @@ import os
 import shutil
 import tempfile
 
-from hamcrest import assert_that
-from hamcrest import has_length
+from hamcrest import none
 from hamcrest import is_in
 from hamcrest import is_not
-from hamcrest import none
+from hamcrest import assert_that
+from hamcrest import has_length
 
 from zope import component
 
@@ -24,26 +24,33 @@ from nti.contentrendering import interfaces
 
 from nti.contentrendering.tests import ContentrenderingLayerTest
 
+
 class TestArchive(ContentrenderingLayerTest):
 
-	def setUp(self):
-		super(TestArchive, self).setUp()
-		self.temp_dir = tempfile.mkdtemp()
+    def setUp(self):
+        super(TestArchive, self).setUp()
+        self.temp_dir = tempfile.mkdtemp()
 
-	def tearDown(self):
-		shutil.rmtree(self.temp_dir)
+    def tearDown(self):
+        shutil.rmtree(self.temp_dir)
 
-	def test_utility(self):
-		u = component.queryUtility(interfaces.IRenderedBookArchiver)
-		assert_that(u, is_not(none()))
+    def test_utility(self):
+        u = component.queryUtility(interfaces.IRenderedBookArchiver)
+        assert_that(u, is_not(none()))
 
-	def test_archive_biology(self):
-		source_path = os.path.join(os.path.dirname(__file__), 'intro-biology-rendered-book')
-		added = archive._archive(source_path, self.temp_dir)
-		assert_that(added, has_length(40))
-		assert_that('intro-biology-rendered-book/eclipse-toc.xml', is_in(added))
-		assert_that('intro-biology-rendered-book/icons/chapters/C2.png', is_in(added))
-		assert_that('intro-biology-rendered-book/images/chapters/C2.png', is_in(added))
-		assert_that('intro-biology-rendered-book/js/worksheet.js', is_in(added))
-		assert_that('intro-biology-rendered-book/styles/prealgebra.css', is_in(added))
-		assert_that('intro-biology-rendered-book/.version', is_in(added))
+    def test_archive_biology(self):
+        source_path = os.path.join(os.path.dirname(__file__),
+                                   'intro-biology-rendered-book')
+        added = archive._archive(source_path, self.temp_dir)
+        assert_that(added, has_length(40))
+        assert_that('intro-biology-rendered-book/eclipse-toc.xml', 
+					is_in(added))
+        assert_that('intro-biology-rendered-book/icons/chapters/C2.png', 
+					is_in(added))
+        assert_that('intro-biology-rendered-book/images/chapters/C2.png', 
+					is_in(added))
+        assert_that('intro-biology-rendered-book/js/worksheet.js',
+					is_in(added))
+        assert_that('intro-biology-rendered-book/styles/prealgebra.css', 
+					is_in(added))
+        assert_that('intro-biology-rendered-book/.version', is_in(added))
