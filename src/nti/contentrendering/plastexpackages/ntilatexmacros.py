b0da4ae948558565845a8bcd68f0e04595ff41c8
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-06 19:52:10 +0000

    rename module and move renderer
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentrendering/trunk@108101 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentrendering/plastexpackages/ntilatexmacros.py
+++ src/nti/contentrendering/plastexpackages/ntilatexmacros.py
@@ -32,7 +32,6 @@ from plasTeX.Base import TextCommand
 from plasTeX.Packages.hyperref import href as base_href
 		
 from nti.contentrendering import plastexids
-from nti.contentrendering import interfaces as crd_interfaces
 
 from nti.contentrendering.plastexpackages._util import _OneText
 from nti.contentrendering.plastexpackages._util import LocalContentMixin
@@ -41,8 +40,6 @@ from nti.contentrendering.plastexpackages._util import incoming_sources_as_plain
 from nti.contentrendering.plastexpackages.graphicx import includegraphics
 
 from nti.contentrendering.resources import interfaces as resource_interfaces
-
-from nti.ntiids import ntiids
 
 # Monkey patching time
 # SAJ: The following are set to render properly nested HTML.
@@ -480,269 +477,17 @@ from nti.contentrendering.plastexpackages.nticard import nticardname
 nticard = nticard
 nticardname = nticardname
 
+# Relatedwork
 
-###############################################################################
-# The following block of commands concern representing related readings
-###############################################################################
+from nti.contentrendering.plastexpackages.ntirelatedwork import relatedwork
+from nti.contentrendering.plastexpackages.ntirelatedwork import relatedworkref
+from nti.contentrendering.plastexpackages.ntirelatedwork import relatedworkname
+from nti.contentrendering.plastexpackages.ntirelatedwork import relatedworkrefname
 
-class relatedworkname(Base.Command):
-	pass
-
-@interface.implementer(crd_interfaces.IEmbeddedContainer)
-class relatedwork(LocalContentMixin, Base.Environment, plastexids.NTIIDMixin):
-	args = '[ options:dict ]'
-
-	# Only classes with counters can be labeled, and \label sets the
-	# id property, which in turn is used as part of the NTIID (when no NTIID is set explicitly)
-	counter = 'relatedwork'
-	blockType = True
-	_ntiid_cache_map_name = '_relatedwork_ntiid_map'
-	_ntiid_allow_missing_title = False
-	_ntiid_suffix = 'relatedwork.'
-	_ntiid_title_attr_name = 'ref' # Use our counter to generate IDs if no ID is given
-	_ntiid_type = 'RelatedWork'
-
-	#: From IEmbeddedContainer
-	mimeType = "application/vnd.nextthought.relatedwork"
-	targetMimeType = "application/vnd.nextthought.content"
-	icon = None
-	iconResource = None
-	target_ntiid = None
-	_description = None
-	_uri = u''
-
-	class worktitle(Base.Command):
-		args = 'title'
-
-		def digest(self, tokens):
-			tok = super(relatedwork.worktitle,self).digest(tokens)
-			self.parentNode.title = self.attributes['title']
-			return tok
-
-	class workcreator(Base.Command):
-		args = 'creator'
-
-		def digest(self, tokens):
-			tok = super(relatedwork.workcreator,self).digest(tokens)
-			self.parentNode.creator = self.attributes['creator']
-			return tok
-
-	class worksource(Base.Command):
-		args = 'uri:url'
-
-		tokens = ( 	( ' ', '' ), ( '\\&', '&' ), ( '\\_', '_' ), ( '\\%', '%' ), 
-					(u'\u2013', u'--'), (u'\u2014', u'---') )
-		
-		def replacer(self, source):
-			for a, b in self.tokens:
-				source = source.replace(a, b)
-			return source
-
-		def digest(self, tokens):
-			tok = super(relatedwork.worksource,self).digest(tokens)
-			self.attributes['uri'] = self.replacer(self.attributes['uri'].source)
-			return tok
-
-	class worksourceref(Base.Crossref.ref):
-		args = 'target:idref'
-
-	def digest(self, tokens):
-		tok = super(relatedwork,self).digest(tokens)
-
-		options = self.attributes.get( 'options', {} ) or {}
-		self.visibility = 'everyone'
-		if 'visibility' in options.keys():
-			self.visibility = options['visibility']
-
-		self.target_ntiid = None
-		self.targetMimeType = None
-
-		icons = self.getElementsByTagName('includegraphics')
-		if icons:
-			self.iconResource = icons[0]
-
-		return tok
-
-	@readproperty
-	def description(self):
-		if self._description is None:
-			self._description = TeXFragment()
-			self._description.parentNode = self
-			self._description.ownerDocument = self.ownerDocument
-			node_types = ['label', 'worktitle', 'workcreator', 'worksource', 'worksourceref', 'includegraphics']
-			for child in self.childNodes:
-				if child.nodeName not in node_types:
-					self._description.appendChild(child)
-		return self._description
-
-	@readproperty
-	def uri(self):
-		if not self._uri:
-			worksources = self.getElementsByTagName('worksource')
-			if worksources:
-				self._uri = worksources[0].attributes.get('uri')
-			else:
-				worksources = self.getElementsByTagName('worksourceref')
-				if worksources:
-					if hasattr(worksources[0].idref['target'], 'ntiid'):
-						self._uri = worksources[0].idref['target'].ntiid
-		return self._uri
-
-	def gen_target_ntiid(self):
-		from nti.ntiids.ntiids import is_valid_ntiid_string
-
-		uri = self.uri
-		if is_valid_ntiid_string( uri ):
-			self.target_ntiid = uri
-			self.targetMimeType = 'application/vnd.nextthought.content'
-			ntiid_specific = ntiids.get_specific( uri )
-			self.icon = '/'.join([u'..', ntiid_specific.split('.')[0], 'icons', 'chapters', 'generic_book.png'])
-
-		else:
-			from nti.ntiids.ntiids import make_ntiid, TYPE_UUID
-			from hashlib import md5
-			# TODO: Hmm, what to use as the provider? Look for a hostname in the
-			# URL?
-			self.target_ntiid = make_ntiid( provider='NTI',
-											nttype=TYPE_UUID,
-											specific=md5(uri).hexdigest() )
-			self.targetMimeType = 'application/vnd.nextthought.externallink'
-
-class relatedworkrefname(Base.Command):
-	pass
-
-@interface.implementer(crd_interfaces.IEmbeddedContainer)
-class relatedworkref(Base.Crossref.ref, plastexids.NTIIDMixin):
-	args = '[ options:dict ] label:idref uri:url desc < NTIID:str >'
-
-	counter = 'relatedworkref'
-	blockType = True
-	_ntiid_cache_map_name = '_relatedworkref_ntiid_map'
-	_ntiid_allow_missing_title = False
-	_ntiid_suffix = 'relatedworkref.'
-	_ntiid_title_attr_name = 'label'
-	_ntiid_type = 'RelatedWorkRef'
-
-	#: From IEmbeddedContainer
-	mimeType = "application/vnd.nextthought.relatedworkref"
-	_targetMimeType = None
-	_target_ntiid = None
-
-	def digest(self, tokens):
-		tok = super(relatedworkref, self).digest(tokens)
-
-		self._options = self.attributes.get( 'options', {} ) or {}
-		self.attributes['nti-requirements'] = u''
-		requirements = self._options.get('nti-requirements', u'').split()
-		for requirement in requirements:
-			if requirement == u'flash':
-				requirement = u'mime-type:application/x-shockwave-flash'
-			self.attributes['nti-requirements'] = ' '.join([self.attributes['nti-requirements'], requirement])
-		self.attributes['nti-requirements'] = self.attributes['nti-requirements'].strip()
-		if self.attributes['nti-requirements'] == u'':
-			self.attributes['nti-requirements'] = None
-		self.label = self.attributes.get('label')
-
-		self._uri = self.attributes['uri']
-		if hasattr(self._uri, 'source'):
-			self._uri = self._uri.source.replace(' ', '') \
-										.replace('\\&', '&') \
-										.replace('\\_', '_') \
-										.replace('\\%', '%') \
-										.replace(u'\u2013', u'--') \
-										.replace(u'\u2014', u'---')
-		self.relatedwork = self.idref['label']
-		self._creator = None
-		self._description = None
-		self._title = None
-
-		# Remove the empty NTIID key so auto NTIID generation works
-
-		# SAJ: It is a hack to have this code here. The code in
-		# contentrendering.platexids should account for the possibility that the
-		# value of the 'NTIID' key could be 'None', however I have not evaluated what
-		# undesired side affects might come from changing the code in
-		# contentrendering.plastexids.
-		if 'NTIID' in self.attributes and self.attributes['NTIID'] is None:
-			del self.attributes['NTIID']
-
-		self._target_ntiid = None
-
-		return tok
-
-	@readproperty
-	def category(self):
-		return self._options.get('category') or u'required'
-
-	@readproperty
-	def creator(self):
-		if self._creator is None:
-			return self.relatedwork.creator
-		return self._creator
-
-	@readproperty
-	def description(self):
-		description = self.attributes.get('desc')
-		if len(description.childNodes) == 0:
-			return self.relatedwork.description
-		return description
-
-	@readproperty
-	def icon(self):
-		if self.relatedwork.iconResource is not None:
-			return self.relatedwork.iconResource.image.url
-		elif self.relatedwork.icon is not None:
-			return self.relatedwork.icon
-		else:
-			return ''
-
-	@readproperty
-	def target_ntiid(self):
-		if self._target_ntiid is None:
-			self.gen_target_ntiid()
-		return self._target_ntiid
-
-	@readproperty
-	def targetMimeType(self):
-		if self._targetMimeType is None:
-			self.gen_target_ntiid()
-		return self._targetMimeType
-
-	@readproperty
-	def title(self):
-		if self._title is None:
-			return self.relatedwork.title
-		return self._title
-
-	@readproperty
-	def uri(self):
-		if self._uri == '' or self._uri is None:
-			return self.relatedwork.uri
-		return self._uri
-
-	@readproperty
-	def visibility(self):
-		visibility = self._options.get('visibility')
-		if visibility == '' or visibility is None:
-			return self.relatedwork.visibility
-		return visibility
-
-	def gen_target_ntiid(self):
-		from nti.ntiids.ntiids import is_valid_ntiid_string
-
-		uri = self.uri
-		if is_valid_ntiid_string( uri ):
-			self._target_ntiid = uri
-			self._targetMimeType = 'application/vnd.nextthought.content'
-		else:
-			from nti.ntiids.ntiids import make_ntiid, TYPE_UUID
-			from hashlib import md5
-			# TODO: Hmm, what to use as the provider? Look for a hostname in the
-			# URL?
-			self._target_ntiid = make_ntiid( provider='NTI',
-											nttype=TYPE_UUID,
-											specific=md5(uri).hexdigest() )
-			self._targetMimeType = 'application/vnd.nextthought.externallink'
+relatedwork = relatedwork
+relatedworkref = relatedworkref
+relatedworkname = relatedworkname
+relatedworkrefname = relatedworkrefname
 
 ###############################################################################
 # The following block of commands concern representing forum discussions.
