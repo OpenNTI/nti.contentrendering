92f9aeb2f7ddaa20c166aeb60229f4ce2f80cdc0
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-08 20:12:29 +0000

    add ntititlelesssection
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentrendering/trunk@108278 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/contentrendering/plastexpackages/ntisectioning.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Define macros for presenting TimelineJS
+
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+
+from plasTeX.Base.LaTeX.Sectioning import section
+
+
+class ntititlelesssection(section):
+    pass
