8fcb96079c9deee506baccf08811eadc6439b2e0
Carlos Sanchez <papachoco@gmail.com>
2017-10-16 20:55:34 -0500

    pep8; remove unicode literals

--- src/nti/contentrendering/traversing.py
+++ src/nti/contentrendering/traversing.py
@@ -7,14 +7,15 @@ content rendering process.
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
-__docformat__ = "restructuredtext en"
-
-logger = __import__('logging').getLogger(__name__)
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
 from zope.location.interfaces import LocationError
 
 from zope.traversing.adapters import DefaultTraversable
+
+logger = __import__('logging').getLogger(__name__)
 
 
 class PlastexTraverser(DefaultTraversable):
