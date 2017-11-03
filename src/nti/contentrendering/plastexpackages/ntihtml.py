50e1063e86fb5cd32f7090bb2dad893e0500e0ff
Carlos Sanchez <papachoco@gmail.com>
2017-11-03 04:37:41 -0500

    add helper method to set html text

--- src/nti/contentrendering/plastexpackages/ntihtml.py
+++ src/nti/contentrendering/plastexpackages/ntihtml.py
@@ -8,10 +8,15 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
+from plasTeX.DOM import Text
+
 from plasTeX.Packages.html import rawhtml
 
 logger = __import__('logging').getLogger(__name__)
 
 
 class ntirawhtml(rawhtml):
-    pass
+
+    def set_html(self, html):
+        self.unicode = Text(html)
+        self.unicode.isMarkup = True
