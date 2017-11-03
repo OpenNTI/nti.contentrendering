035b23136bdfef4c2e7ca6eed1dcf3681b9ec5aa
Carlos Sanchez <papachoco@gmail.com>
2017-11-03 05:19:02 -0500

    alias isMarkup property

--- src/nti/contentrendering/plastexpackages/ntihtml.py
+++ src/nti/contentrendering/plastexpackages/ntihtml.py
@@ -8,14 +8,22 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
-from plasTeX.DOM import Text
+from plasTeX.DOM import Text as BaseText
 
 from plasTeX.Packages.html import rawhtml
+
+from nti.property.property import alias
 
 logger = __import__('logging').getLogger(__name__)
 
 
+class Text(BaseText):
+    isMarkup = alias('is_markup')
+
+
 class ntirawhtml(rawhtml):
+
+    isMarkup = True
 
     def set_html(self, html):
         self.unicode = Text(html)
