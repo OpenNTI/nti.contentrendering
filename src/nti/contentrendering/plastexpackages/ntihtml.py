3d66df88c2237aba2d3b8872239679849cc55a9a
Carlos Sanchez <papachoco@gmail.com>
2017-11-14 16:38:29 -0600

    add ntiblockquote plasTeX enviroment

--- src/nti/contentrendering/plastexpackages/ntihtml.py
+++ src/nti/contentrendering/plastexpackages/ntihtml.py
@@ -8,6 +8,8 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
+from plasTeX import Environment
+
 from plasTeX.DOM import Text as BaseText
 
 from plasTeX.Packages.html import rawhtml
@@ -28,3 +30,7 @@ class ntirawhtml(rawhtml):
     def set_html(self, html):
         self.unicode = Text(html)
         self.unicode.isMarkup = True
+
+
+class ntiblockquote(Environment):
+    blockType = True
