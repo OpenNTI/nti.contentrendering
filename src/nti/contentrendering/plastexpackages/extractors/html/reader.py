9be1a829ea193ecc0ad134baffb27db170f50043
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-10-05 17:00:36 -0500

    pep8; remove unicode literals

--- src/nti/contentrendering/plastexpackages/extractors/html/reader.py
+++ src/nti/contentrendering/plastexpackages/extractors/html/reader.py
@@ -4,30 +4,32 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
-__docformat__ = "restructuredtext en"
-
-logger = __import__('logging').getLogger(__name__)
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
 import os
 import codecs
 
 from lxml import html
 
+logger = __import__('logging').getLogger(__name__)
 
-class HTMLReader():
-	def __init__(self, inputfile):
-		self.inputfile = inputfile
-		self.script = self.get_script()
-		self.element = html.fromstring(self.script)
 
-	def get_script(self):
-		script = None
-		#verify that the input file exists
-		inputfile = os.path.expanduser(self.inputfile)
-		if not os.path.exists(inputfile):
-			logger.warning('The source file %s does not exist',  inputfile)
-		else:
-			with codecs.open(inputfile, 'r', 'utf-8') as fp:
-				script = fp.read()
-		return script
+class HTMLReader(object):
+
+    def __init__(self, inputfile):
+        self.inputfile = inputfile
+        self.script = self.get_script()
+        self.element = html.fromstring(self.script)
+
+    def get_script(self):
+        script = None
+        # verify that the input file exists
+        inputfile = os.path.expanduser(self.inputfile)
+        if not os.path.exists(inputfile):
+            logger.warning('The source file %s does not exist',  inputfile)
+        else:
+            with codecs.open(inputfile, 'r', 'utf-8') as fp:
+                script = fp.read()
+        return script
