ccec0a91c31f8c0d820540f3eb8955e9cc04975d
Egawati Panjei <egawati.panjei@gmail.com>
2018-10-08 10:08:14 +0700

    add test for content unit statistics

--- src/nti/contentrendering/plastexpackages/extractors/statistics.py
+++ src/nti/contentrendering/plastexpackages/extractors/statistics.py
@@ -18,6 +18,10 @@ from zope import interface
 from nti.contentrendering.interfaces import IRenderedBook
 from nti.contentrendering.interfaces import IContentUnitStatistics
 
+from nti.contentrendering.plastexpackages.extractors.html.reader import HTMLReader
+
+from nti.contentrendering.plastexpackages.extractors.html.statistics import HTMLExtractor
+
 logger = __import__('logging').getLogger(__name__)
 
 
@@ -25,12 +29,12 @@ logger = __import__('logging').getLogger(__name__)
 @interface.implementer(IContentUnitStatistics)
 class _ContentUnitStatistics(object):
 
-    def __init__(self, book=None):
-        pass
+    def __init__(self, book=None, lang='en'):
+        self.lang = lang
 
     def transform(self, book, outpath=None):
         outpath = outpath or book.contentLocation
-        outpath = os.path.expanduser(outpath)
+        self.outpath = os.path.expanduser(outpath)
         dom = book.toc.dom
         root = dom.documentElement
         index = {'Items': {}}
@@ -43,8 +47,21 @@ class _ContentUnitStatistics(object):
             element_index = index[ntiid] = {}
             element_index['NTIID'] = ntiid
             element_index['href'] = node.getAttributeNode('href').value
+            if u'#' not in element_index['href']:
+                element_index['data']={}
+                html_element = self._read_html(element_index['href'])
+                extractor = HTMLExtractor(html_element, self.lang)
+                element_index['data']['number_of_paragraphs'] = extractor.number_paragraph
+                element_index['data']['number_of_words'] = extractor.total_number_of_words()
+                element_index['data']['number_of_sentences'] = extractor.number_sentence
         if node.hasChildNodes():
             for child in node.childNodes:
                 if child.nodeName == 'topic':
                     containing_index = element_index.setdefault('Items', {})
                     self._process_topic(child, containing_index)
+
+    def _read_html(self, name):
+        filename = os.path.join(os.path.dirname(__file__), self.outpath, name)
+        reader = HTMLReader(filename)
+        element = reader.element
+        return element
