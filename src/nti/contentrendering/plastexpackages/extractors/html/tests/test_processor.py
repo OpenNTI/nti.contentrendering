efb865926a26236bc2eab4f15e568ed8eb5b1b7a
Egawati Panjei <egawati.panjei@gmail.com>
2018-10-22 18:46:51 +0700

    update status

--- src/nti/contentrendering/plastexpackages/extractors/html/tests/test_processor.py
+++ src/nti/contentrendering/plastexpackages/extractors/html/tests/test_processor.py
@@ -25,20 +25,20 @@ class HTMLProcessTest(HTMLExtractorTests):
         element = html.fromstring(script)
         text = process_element(u"", element)
         assert_that(text, 
-					is_(u'We recommend the following specifications to ensure the highest quality experience while using NextThought: \n'))
+					is_(u'We recommend the following specifications to ensure the highest quality experience while using NextThought:\n'))
 
     def test_bolded_paragraph(self):
         script = u"""<body><div><p class="par" id="ac446671f9623e95ea098e5d0777dc39"><b class="bfseries">Can everyone see the note I just made?</b><br /></p></div></body>"""
         element = html.fromstring(script)
         text = process_element(u"", element)
-        assert_that(text, is_(u'Can everyone see the note I just made?\n\n'))
+        assert_that(text, is_(u'Can everyone see the note I just made?\n'))
 
     def test_bolded_paragraph_with_tail(self):
         script = u"""<body><div><p class="par" id="ac446671f9623e95ea098e5d0777dc39"><b class="bfseries">Can everyone see the note I just made?</b> This is a tail<br /></p></div></body>"""
         element = html.fromstring(script)
         text = process_element(u"", element)
         assert_that(
-            text, is_(u'Can everyone see the note I just made? This is a tail\n\n'))
+            text, is_(u'Can everyone see the note I just made? This is a tail\n'))
 
     def test_paragraph(self):
         script = u"""<body><div class="section title">System Requirements</div> <p class="placeholder"></p> <p class="par"> <a name="section:System Requirements"></a> </p> <a name="cea4cf4b4bfca0e0912c8b9308ae9c64"></a> <p class="par" id="cea4cf4b4bfca0e0912c8b9308ae9c64">We recommend the following specifications to ensure the highest quality experience while using NextThought: </p> <a name="45bb5b2ce7b3402678006a7b9b89c5e8"></a> <p class="par" id="45bb5b2ce7b3402678006a7b9b89c5e8">1Mb/s+ broadband Internet connection for optimal video viewing A computer with at least 2GB memory and minimum screen resolution of 1024x768 <a href="http://get.adobe.com/reader/">Adobe Reader</a> or similar PDF reading software </p> </div></body>"""
@@ -46,7 +46,7 @@ class HTMLProcessTest(HTMLExtractorTests):
         html_obj = HTMLSample()
         text = process_element(u"", element, html_obj)
         assert_that(html_obj.number_paragraph, is_(2))
-        assert_that(text, is_(u'System Requirements \n \n   \n We recommend the following specifications to ensure the highest quality experience while using NextThought:  \n 1Mb/s+ broadband Internet connection for optimal video viewing A computer with at least 2GB memory and minimum screen resolution of 1024x768 Adobe Reader or similar PDF reading software  \n'))
+        assert_that(text, is_(u'System Requirements\n We recommend the following specifications to ensure the highest quality experience while using NextThought:\n 1Mb/s+ broadband Internet connection for optimal video viewing A computer with at least 2GB memory and minimum screen resolution of 1024x768 Adobe Reader or similar PDF reading software\n'))
 
     def test_sidebar_without_title(self):
         script = u"""<body><div><div class="sidebar" data-ntiid="tag:nextthought.com,2011-10:IFSTA-HTML-sample_book.sidebar.0" ntiid="tag:nextthought.com,2011-10:IFSTA-HTML-sample_book.sidebar.0">
@@ -107,4 +107,4 @@ class HTMLProcessTest(HTMLExtractorTests):
         text = process_element(u"", element, html_obj)
         assert_that(html_obj.number_paragraph, is_(2))
         assert_that(html_obj.number_sidebar, is_(0))
-        assert_that(text, is_(u' General Features \n \n  You control who is able to see your note. To change who your note is shared with or add someone to the note, begin typing a contact, list, or group\u2019s name in the sharing field and click on them to share.  \n To remove a person, group, or list, hover over their name in the sharing field, and an \u201cx\u201d will appear. Click the x to remove. If no one is listed in the sharing field, the note will be private and only you can access it.  \n '))
+        assert_that(text, is_(u' General Features\nYou control who is able to see your note. To change who your note is shared with or add someone to the note, begin typing a contact, list, or group\u2019s name in the sharing field and click on them to share.\n To remove a person, group, or list, hover over their name in the sharing field, and an \u201cx\u201d will appear. Click the x to remove. If no one is listed in the sharing field, the note will be private and only you can access it.\n '))
